1f2379503d46d40b95b2ead71d933ad5dd053383
Jeremy Rickard <jeremy.rickard@microsoft.com>
2018-12-05 23:08:20 -0700

    Handle error in output

--- pkg/mixin/helm/outputs.go
+++ pkg/mixin/helm/outputs.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"path/filepath"
 
+	errWrap "github.com/pkg/errors"
+
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -20,9 +22,12 @@ func (m *Mixin) getSecret(namespace, name, key string) (string, error) {
 
 	config, err := clientcmd.BuildConfigFromFlags("", kubecfg)
 	if err != nil {
-		return "", fmt.Errorf("couldn't build kubernetes client: %s", err)
+		return "", fmt.Errorf("couldn't build kubernetes config: %s", err)
 	}
 	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return "", errWrap.Wrap(err, "couldn't create kubernetes client")
+	}
 	secret, err := clientset.CoreV1().Secrets(namespace).Get(name, metav1.GetOptions{})
 	if errors.IsNotFound(err) {
 		return "", fmt.Errorf("error getting secret %s from namespace %s: %s", name, namespace, err)
