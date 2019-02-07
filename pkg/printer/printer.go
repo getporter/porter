73d2313d26eec995e9886520bdb12804a70381d0
Vaughn Dice <vadice@microsoft.com>
2019-02-06 18:54:10 -0700

    change 'FormatRaw' to 'FormatPlaintext'

--- pkg/printer/printer.go
+++ pkg/printer/printer.go
@@ -5,16 +5,16 @@ import "github.com/pkg/errors"
 type Format string
 
 const (
-	FormatJson  Format = "json"
-	FormatTable Format = "table"
-	FormatYaml  Format = "yaml"
-	FormatRaw   Format = "raw"
+	FormatJson      Format = "json"
+	FormatTable     Format = "table"
+	FormatYaml      Format = "yaml"
+	FormatPlaintext Format = "plaintext"
 )
 
 func ParseFormat(v string) (Format, error) {
 	format := Format(v)
 	switch format {
-	case FormatTable, FormatJson, FormatYaml, FormatRaw:
+	case FormatTable, FormatJson, FormatYaml, FormatPlaintext:
 		return format, nil
 	default:
 		return "", errors.Errorf("invalid format: %s", v)
