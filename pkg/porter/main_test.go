5e72fb29fd010ebb39fc234ba60826ede5eb0b47
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2019-02-21 19:48:35 -0600

    Define TestMain's in other packages
    This lets us mock out calling executables from our unit tests.
    I'm working on making this not needed. But it may span multiple PRs as I split
    out the MixinProvider interface from the Porter app

---
+++ pkg/porter/main_test.go
@@ -0,0 +1,12 @@
+package porter
+
+import (
+	"testing"
+
+	"github.com/deislabs/porter/pkg/test"
+)
+
+// sad hack: not sure how to make a common test main for all my subpackages
+func TestMain(m *testing.M) {
+	test.TestMainWithMockedCommandHandlers(m)
+}
