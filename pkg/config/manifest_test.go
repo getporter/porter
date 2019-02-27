227b5360dd7c8f641644986236c994f8a814a39b
Urvashi Reddy <ureddy@pivotal.io>
2019-02-27 13:53:20 -0800

    Skip check for local dependency bundle with url paths
    - Change example porter file used for tests

--- pkg/config/manifest_test.go
+++ pkg/config/manifest_test.go
@@ -10,19 +10,20 @@ import (
 
 func TestReadManifest_URL(t *testing.T) {
 	c := NewTestConfig(t)
-	url := "https://raw.githubusercontent.com/deislabs/porter/master/pkg/config/testdata/porter.yaml"
+	url := "https://raw.githubusercontent.com/deislabs/porter/master/pkg/config/testdata/simple.porter.yaml"
 	m, err := c.ReadManifest(url)
 
 	require.NoError(t, err)
+	assert.Equal(t, "hello", m.Name)
 	assert.Equal(t, url, m.path)
 }
 
 func TestReadManifest_Validate_InvalidURL(t *testing.T) {
 	c := NewTestConfig(t)
-	_, err := c.ReadManifest("http://fake-example=porter")
+	_, err := c.ReadManifest("http://fake-example-porter")
 
 	assert.Error(t, err)
-	assert.Regexp(t, "could not reach url http://fake-example=porter", err)
+	assert.Regexp(t, "could not reach url http://fake-example-porter", err)
 }
 
 func TestReadManifest_File(t *testing.T) {
