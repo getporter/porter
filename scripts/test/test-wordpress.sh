6b5dd6caf45cc1a2f12bb4000e12b3edbc5b7a38
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2019-03-26 13:02:08 -0500

    Don't specify a claim when installing wordpress

--- scripts/test/test-wordpress.sh
+++ scripts/test/test-wordpress.sh
@@ -14,5 +14,5 @@ cp build/testdata/bundles/wordpress/porter.yaml .
 sed -i "s/porter-wordpress:latest/${REGISTRY}\/porter-wordpress:latest/g" porter.yaml
 
 ${PORTER_HOME}/porter build
-${PORTER_HOME}/porter install PORTER-WORDPRESS -f bundle.json --cred ci --insecure
+${PORTER_HOME}/porter install --insecure --cred ci
 cat ${PORTER_HOME}/claims/wordpress.json
