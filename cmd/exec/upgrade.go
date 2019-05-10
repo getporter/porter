d8d99a8c6084ea42ebf8d5e83841fc42be090cec
Vaughn Dice <vadice@microsoft.com>
2019-05-10 11:51:20 -0600

    ref(*): remove use of anonymous options where applicable

--- cmd/exec/upgrade.go
+++ cmd/exec/upgrade.go
@@ -6,17 +6,16 @@ import (
 )
 
 func buildUpgradeCommand(m *exec.Mixin) *cobra.Command {
-	var opts struct {
-		file string
-	}
+	opts := exec.UpgradeOptions{}
+
 	cmd := &cobra.Command{
 		Use:   "upgrade",
 		Short: "Execute the upgrade functionality of this mixin",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return m.Upgrade(opts.file)
+			return m.Upgrade(opts.File)
 		},
 	}
 	flags := cmd.Flags()
-	flags.StringVarP(&opts.file, "file", "f", "", "Path to the script to execute")
+	flags.StringVarP(&opts.File, "file", "f", "", "Path to the script to execute")
 	return cmd
 }
--- pkg/exec/install.go
+++ pkg/exec/install.go
@@ -1,5 +1,10 @@
 package exec
 
+// InstallOptions represent the options for an exec mixin install action
+type InstallOptions struct {
+	File string
+}
+
 func (m *Mixin) Install(commandFile string) error {
 	err := m.loadAction(commandFile)
 	if err != nil {
