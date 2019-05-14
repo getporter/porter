82305ca39414b90167b963855fa6dd93d5b7a0cd
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2019-05-14 11:13:22 -0500

    Add canary tests to the mixin feed test suite
    I've added tests to ensure that having canary (a non-semver) in the
    mixin feed didn't cause any regressions.

--- pkg/mixin/feed/feed_test.go
+++ pkg/mixin/feed/feed_test.go
@@ -12,6 +12,10 @@ func TestMixinFeed_Search_Latest(t *testing.T) {
 	f := NewMixinFeed(tc.Context)
 
 	f.Index["helm"] = make(map[string]*MixinFileset)
+	f.Index["helm"]["canary"] = &MixinFileset{
+		Mixin: "helm",
+		Version:"canary",
+	}
 	f.Index["helm"]["v1.2.3"] = &MixinFileset{
 		Mixin: "helm",
 		Version:"v1.2.3",
@@ -26,4 +30,25 @@ func TestMixinFeed_Search_Latest(t *testing.T) {
 	require.NotNil(t, result)
 
 	assert.Equal(t, "v1.2.4", result.Version)
+}
+
+func TestMixinFeed_Search_Canary(t *testing.T) {
+	tc := context.NewTestContext(t)
+	f := NewMixinFeed(tc.Context)
+
+	f.Index["helm"] = make(map[string]*MixinFileset)
+	f.Index["helm"]["canary"] = &MixinFileset{
+		Mixin: "helm",
+		Version:"canary",
+	}
+	f.Index["helm"]["v1.2.4"] = &MixinFileset{
+		Mixin: "helm",
+		Version:"v1.2.4",
+	}
+
+	result := f.Search("helm", "canary")
+
+	require.NotNil(t, result)
+
+	assert.Equal(t, "canary", result.Version)
 }
