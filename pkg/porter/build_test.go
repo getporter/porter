96006a5cd1b78a9b30a3d02ed330da21ce6ca38c
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2019-06-19 08:56:22 -0700

    Add ImageMap (#419)
    * Add ImageMap
    
    * Add test for missing platform

--- pkg/porter/build_test.go
+++ pkg/porter/build_test.go
@@ -233,3 +233,73 @@ func TestPorter_paramRequired(t *testing.T) {
 	require.True(t, ok)
 	require.True(t, p2.Required)
 }
+
+func TestPorter_generateImages(t *testing.T) {
+	p := NewTestPorter(t)
+	p.TestConfig.SetupPorterHome()
+
+	configTpl, err := p.Templates.GetManifest()
+	require.Nil(t, err)
+	p.TestConfig.TestContext.AddTestFileContents(configTpl, config.Name)
+
+	err = p.LoadManifest()
+	require.NoError(t, err)
+
+	mappedImage := config.MappedImage{
+		Description:   "un petite server",
+		Image:         "deislabs/myserver:1.0.0",
+		ImageType:     "docker",
+		Digest:        "abc123",
+		Size:          12,
+		MediaType:     "download",
+		OriginalImage: "deis/myserver:1.0.0",
+		Platform: &config.ImagePlatform{
+			OS:           "linux",
+			Architecture: "amd64",
+		},
+	}
+	p.Manifest.ImageMap = map[string]config.MappedImage{
+		"server": mappedImage,
+	}
+
+	images := p.generateBundleImages()
+
+	require.Len(t, images, 1)
+	img := images["server"]
+	assert.Equal(t, mappedImage.Description, img.Description)
+	assert.Equal(t, mappedImage.Image, img.Image)
+	assert.Equal(t, mappedImage.ImageType, img.ImageType)
+	assert.Equal(t, mappedImage.Digest, img.Digest)
+	assert.Equal(t, mappedImage.Size, img.Size)
+	assert.Equal(t, mappedImage.MediaType, img.MediaType)
+	assert.Equal(t, mappedImage.OriginalImage, img.OriginalImage)
+	assert.Equal(t, mappedImage.Platform.OS, img.Platform.OS)
+	assert.Equal(t, mappedImage.Platform.Architecture, img.Platform.Architecture)
+}
+
+func TestPorter_generateBundleImages_EmptyPlatform(t *testing.T) {
+	p := NewTestPorter(t)
+	p.TestConfig.SetupPorterHome()
+
+	configTpl, err := p.Templates.GetManifest()
+	require.Nil(t, err)
+	p.TestConfig.TestContext.AddTestFileContents(configTpl, config.Name)
+
+	err = p.LoadManifest()
+	require.NoError(t, err)
+
+	mappedImage := config.MappedImage{
+		Description: "un petite server",
+		Image:       "deislabs/myserver:1.0.0",
+		ImageType:   "docker",
+		Platform:    nil,
+	}
+	p.Manifest.ImageMap = map[string]config.MappedImage{
+		"server": mappedImage,
+	}
+
+	images := p.generateBundleImages()
+	require.Len(t, images, 1)
+	img := images["server"]
+	assert.Nil(t, img.Platform)
+}
