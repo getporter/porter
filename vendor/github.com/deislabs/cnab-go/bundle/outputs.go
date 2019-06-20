37dd92031bc3be2671ffc81b50b97413a27cdc56
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2019-06-20 13:24:05 -0500

    Sync vendor

--- vendor/github.com/deislabs/cnab-go/bundle/outputs.go
+++ vendor/github.com/deislabs/cnab-go/bundle/outputs.go
@@ -1,20 +1,12 @@
 package bundle
 
 type OutputsDefinition struct {
-	Fields   map[string]OutputDefinition `json:"fields" mapstructure:"fields"`
-	Required []string                    `json:"required,omitempty" mapstructure:"required,omitempty"`
+	Fields map[string]OutputDefinition `json:"fields" mapstructure:"fields"`
 }
 
 type OutputDefinition struct {
-	DataType      string             `json:"type" mapstructure:"type"`
-	Default       interface{}        `json:"default,omitempty" mapstructure:"default"`
-	AllowedValues []interface{}      `json:"allowedValues,omitempty" mapstructure:"allowedValues"`
-	MinValue      *int               `json:"minValue,omitempty" mapstructure:"minValue"`
-	MaxValue      *int               `json:"maxValue,omitempty" mapstructure:"maxValue"`
-	MinLength     *int               `json:"minLength,omitempty" mapstructure:"minLength"`
-	MaxLength     *int               `json:"maxLength,omitempty" mapstructure:"maxLength"`
-	Metadata      *ParameterMetadata `json:"metadata,omitempty" mapstructure:"metadata"`
-	Path          string             `json:"path,omitemtpty" mapstructure:"path,omitempty"`
-	ApplyTo       []string           `json:"apply-to,omitempty" mapstructure:"apply-to,omitempty"`
-	Sensitive     bool               `json:"sensitive,omitempty" mapstructure:"sensitive,omitempty"`
+	Definition  string   `json:"definition" mapstructure:"definition"`
+	ApplyTo     []string `json:"applyTo,omitempty" mapstructure:"applyTo,omitempty"`
+	Description string   `json:"description,omitempty" mapstructure:"description"`
+	Path        string   `json:"path,omitemtpty" mapstructure:"path,omitempty"`
 }
--- vendor/github.com/docker/cnab-to-oci/converter/convert.go
+++ vendor/github.com/docker/cnab-to-oci/converter/convert.go
@@ -61,9 +61,6 @@ const ( // Descriptor level annotations and values
 
 	// CNABDescriptorComponentNameAnnotation is a decriptor-level annotation specifying the component name
 	CNABDescriptorComponentNameAnnotation = "io.cnab.component.name"
-
-	// CNABDescriptorOriginalNameAnnotation is a decriptor-level annotation specifying the original image name
-	CNABDescriptorOriginalNameAnnotation = "io.cnab.component.original_name"
 )
 
 // GetBundleConfigManifestDescriptor returns the CNAB runtime config manifest descriptor from a OCI index
@@ -101,6 +98,7 @@ func ConvertOCIIndexToBundle(ix *ocischemav1.Index, config *BundleConfig, origin
 	b := &bundle.Bundle{
 		Actions:     config.Actions,
 		Credentials: config.Credentials,
+		Definitions: config.Definitions,
 		Parameters:  config.Parameters,
 		Custom:      config.Custom,
 	}
@@ -187,7 +185,6 @@ func makeManifests(b *bundle.Bundle, targetReference reference.Named, bundleConf
 		image.Annotations = map[string]string{
 			CNABDescriptorTypeAnnotation:          CNABDescriptorTypeComponent,
 			CNABDescriptorComponentNameAnnotation: name,
-			CNABDescriptorOriginalNameAnnotation:  img.OriginalImage,
 		}
 		manifests = append(manifests, image)
 	}
@@ -239,17 +236,15 @@ func parseManifests(descriptors []ocischemav1.Descriptor, into *bundle.Bundle, o
 			if !ok {
 				return fmt.Errorf("component name missing in descriptor %q", d.Digest)
 			}
-			originalName := d.Annotations[CNABDescriptorOriginalNameAnnotation]
 			if into.Images == nil {
 				into.Images = make(map[string]bundle.Image)
 			}
 			into.Images[componentName] = bundle.Image{
 				BaseImage: bundle.BaseImage{
-					Image:         refFamiliar,
-					OriginalImage: originalName,
-					ImageType:     imageType,
-					MediaType:     d.MediaType,
-					Size:          uint64(d.Size),
+					Image:     refFamiliar,
+					ImageType: imageType,
+					MediaType: d.MediaType,
+					Size:      uint64(d.Size),
 				},
 			}
 		default:
