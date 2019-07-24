04733250d8fc9fcd6ed243f05d0151a6a16a2e72
Ben Wilkinson <benwilk@microsoft.com>
2019-07-24 11:45:56 -0700

    removed iex (invoke-expression) from windows install script

--- scripts/install/install-windows.ps1
+++ scripts/install/install-windows.ps1
@@ -7,13 +7,13 @@ mkdir -f $PORTER_HOME
 
 (new-object System.Net.WebClient).DownloadFile("$PORTER_URL/$PORTER_VERSION/porter-windows-amd64.exe", "$PORTER_HOME\porter.exe")
 (new-object System.Net.WebClient).DownloadFile("$PORTER_URL/$PORTER_VERSION/porter-linux-amd64", "$PORTER_HOME\porter-runtime")
-echo "Installed $(iex "$PORTER_HOME\porter.exe version")"
+echo "Installed $(& $PORTER_HOME\porter.exe version)"
 
 $FEED_URL="$PORTER_URL/atom.xml"
-iex "$PORTER_HOME/porter mixin install exec --version $PORTER_VERSION --feed-url $FEED_URL"
-iex "$PORTER_HOME/porter mixin install kubernetes --version $PORTER_VERSION --feed-url $FEED_URL"
-iex "$PORTER_HOME/porter mixin install helm --version $PORTER_VERSION --feed-url $FEED_URL"
-iex "$PORTER_HOME/porter mixin install azure --version $PORTER_VERSION --feed-url $FEED_URL"
+& $PORTER_HOME/porter mixin install exec --version $PORTER_VERSION --feed-url $FEED_URL
+& $PORTER_HOME/porter mixin install kubernetes --version $PORTER_VERSION --feed-url $FEED_URL
+& $PORTER_HOME/porter mixin install helm --version $PORTER_VERSION --feed-url $FEED_URL
+& $PORTER_HOME/porter mixin install azure --version $PORTER_VERSION --feed-url $FEED_URL
 
 echo "Installation complete."
 echo "Add porter to your path by running:"
