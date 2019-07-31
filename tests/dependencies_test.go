21077c09c18820b3e17197abc09a261e49384009
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2019-07-31 10:23:51 -0500

    Impl deps for invoke and improve deps int test

--- tests/dependencies_test.go
+++ tests/dependencies_test.go
@@ -13,6 +13,22 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestDependenciesLifecycle(t *testing.T) {
+	p := porter.NewTestPorter(t)
+	p.SetupIntegrationTest()
+	defer p.CleanupIntegrationTest()
+	p.Debug = false
+
+	namespace := installWordpressBundle(p)
+	defer cleanupWordpressBundle(p)
+
+	upgradeWordpressBundle(p, namespace)
+
+	invokeWordpressBundle(p, namespace)
+
+	uninstallWordpressBundle(p, namespace)
+}
+
 func randomString(len int) string {
 	bytes := make([]byte, len)
 	for i := 0; i < len; i++ {
@@ -56,55 +72,40 @@ func installWordpressBundle(p *porter.TestPorter) (namespace string) {
 	err = p.InstallBundle(installOpts)
 	require.NoError(p.T(), err, "install of root bundle failed")
 
-	return namespace
-}
-
-func TestDependencies_Install(t *testing.T) {
-	p := porter.NewTestPorter(t)
-	p.SetupIntegrationTest()
-	defer p.CleanupIntegrationTest()
-	p.Debug = false
-
-	installWordpressBundle(p)
-
 	// Verify that the dependency claim is present
 	c, err := p.CNAB.FetchClaim("wordpress-mysql")
-	require.NoError(t, err, "could not fetch claim for the dependency")
-	assert.Equal(t, claim.StatusSuccess, c.Result.Status, "the dependency wasn't recorded as being installed successfully")
+	require.NoError(p.T(), err, "could not fetch claim for the dependency")
+	assert.Equal(p.T(), claim.StatusSuccess, c.Result.Status, "the dependency wasn't recorded as being installed successfully")
 
 	// Verify that the bundle claim is present
 	c, err = p.CNAB.FetchClaim("wordpress")
-	require.NoError(t, err, "could not fetch claim for the root bundle")
-	assert.Equal(t, claim.StatusSuccess, c.Result.Status, "the root bundle wasn't recorded as being installed successfully")
+	require.NoError(p.T(), err, "could not fetch claim for the root bundle")
+	assert.Equal(p.T(), claim.StatusSuccess, c.Result.Status, "the root bundle wasn't recorded as being installed successfully")
 
-	// Uninstall the dependency
+	return namespace
+}
+
+func cleanupWordpressBundle(p *porter.TestPorter) {
 	uninstallOpts := porter.UninstallOptions{}
 	uninstallOpts.CredentialIdentifiers = []string{"ci"}
 	uninstallOpts.Tag = p.Manifest.Dependencies["mysql"].Tag
-	err = uninstallOpts.Validate([]string{"wordpress-mysql"}, p.Context)
-	require.NoError(t, err, "validation of uninstall opts failed for dependent bundle")
+	err := uninstallOpts.Validate([]string{"wordpress-mysql"}, p.Context)
+	assert.NoError(p.T(), err, "validation of uninstall opts failed for dependent bundle")
 
 	err = p.UninstallBundle(uninstallOpts)
-	require.NoError(t, err, "uninstall failed for dependent bundle")
+	assert.NoError(p.T(), err, "uninstall failed for dependent bundle")
 
 	// Uninstall the bundle
 	uninstallOpts = porter.UninstallOptions{}
 	uninstallOpts.CredentialIdentifiers = []string{"ci"}
 	err = uninstallOpts.Validate([]string{}, p.Context)
-	require.NoError(t, err, "validation of uninstall opts failed for dependent bundle")
+	assert.NoError(p.T(), err, "validation of uninstall opts failed for dependent bundle")
 
 	err = p.UninstallBundle(uninstallOpts)
-	require.NoError(t, err, "uninstall failed for root bundle")
+	assert.NoError(p.T(), err, "uninstall failed for root bundle")
 }
 
-func TestDependencies_Upgrade(t *testing.T) {
-	p := porter.NewTestPorter(t)
-	p.SetupIntegrationTest()
-	defer p.CleanupIntegrationTest()
-	p.Debug = false
-
-	namespace := installWordpressBundle(p)
-
+func upgradeWordpressBundle(p *porter.TestPorter, namespace string) {
 	upgradeOpts := porter.UpgradeOptions{}
 	upgradeOpts.CredentialIdentifiers = []string{"ci"}
 	upgradeOpts.Params = []string{ // See https://github.com/deislabs/porter/issues/474
@@ -114,51 +115,53 @@ func TestDependencies_Upgrade(t *testing.T) {
 		"mysql#mysql-name=porter-ci-mysql-" + namespace,
 	}
 	err := upgradeOpts.Validate([]string{}, p.Context)
-	require.NoError(t, err, "validation of upgrade opts for root bundle failed")
+	require.NoError(p.T(), err, "validation of upgrade opts for root bundle failed")
 
 	err = p.UpgradeBundle(upgradeOpts)
-	require.NoError(t, err, "upgrade of root bundle failed")
+	require.NoError(p.T(), err, "upgrade of root bundle failed")
 
 	// Verify that the dependency claim is upgraded
 	c, err := p.CNAB.FetchClaim("wordpress-mysql")
-	require.NoError(t, err, "could not fetch claim for the dependency")
-	assert.Equal(t, claim.ActionUpgrade, c.Result.Action, "the dependency wasn't recorded as being upgraded")
-	assert.Equal(t, claim.StatusSuccess, c.Result.Status, "the dependency wasn't recorded as being upgraded successfully")
+	require.NoError(p.T(), err, "could not fetch claim for the dependency")
+	assert.Equal(p.T(), claim.ActionUpgrade, c.Result.Action, "the dependency wasn't recorded as being upgraded")
+	assert.Equal(p.T(), claim.StatusSuccess, c.Result.Status, "the dependency wasn't recorded as being upgraded successfully")
 
 	// Verify that the bundle claim is upgraded
 	c, err = p.CNAB.FetchClaim("wordpress")
-	require.NoError(t, err, "could not fetch claim for the root bundle")
-	assert.Equal(t, claim.ActionUpgrade, c.Result.Action, "the root bundle wasn't recorded as being upgraded")
-	assert.Equal(t, claim.StatusSuccess, c.Result.Status, "the root bundle wasn't recorded as being upgraded successfully")
-
-	// Uninstall the dependency
-	uninstallOpts := porter.UninstallOptions{}
-	uninstallOpts.CredentialIdentifiers = []string{"ci"}
-	uninstallOpts.Tag = p.Manifest.Dependencies["mysql"].Tag
-	err = uninstallOpts.Validate([]string{"wordpress-mysql"}, p.Context)
-	require.NoError(t, err, "validation of uninstall opts failed for dependent bundle")
-
-	err = p.UninstallBundle(uninstallOpts)
-	require.NoError(t, err, "uninstall failed for dependent bundle")
-
-	// Uninstall the bundle
-	uninstallOpts = porter.UninstallOptions{}
-	uninstallOpts.CredentialIdentifiers = []string{"ci"}
-	err = uninstallOpts.Validate([]string{}, p.Context)
-	require.NoError(t, err, "validation of uninstall opts failed for dependent bundle")
-
-	err = p.UninstallBundle(uninstallOpts)
-	require.NoError(t, err, "uninstall failed for root bundle")
+	require.NoError(p.T(), err, "could not fetch claim for the root bundle")
+	assert.Equal(p.T(), claim.ActionUpgrade, c.Result.Action, "the root bundle wasn't recorded as being upgraded")
+	assert.Equal(p.T(), claim.StatusSuccess, c.Result.Status, "the root bundle wasn't recorded as being upgraded successfully")
 }
 
-func TestDependencies_Uninstall(t *testing.T) {
-	p := porter.NewTestPorter(t)
-	p.SetupIntegrationTest()
-	defer p.CleanupIntegrationTest()
-	p.Debug = false
+func invokeWordpressBundle(p *porter.TestPorter, namespace string) {
+	invokeOpts := porter.InvokeOptions{Action: "ping"}
+	invokeOpts.CredentialIdentifiers = []string{"ci"}
+	invokeOpts.Params = []string{ // See https://github.com/deislabs/porter/issues/474
+		"wordpress-password=mypassword",
+		"namespace=" + namespace,
+		"wordpress-name=porter-ci-wordpress-" + namespace,
+		"mysql#mysql-name=porter-ci-mysql-" + namespace,
+	}
+	err := invokeOpts.Validate([]string{}, p.Context)
+	require.NoError(p.T(), err, "validation of invoke opts for root bundle failed")
 
-	namespace := installWordpressBundle(p)
+	err = p.InvokeBundle(invokeOpts)
+	require.NoError(p.T(), err, "invoke of root bundle failed")
 
+	// Verify that the dependency claim is invoked
+	c, err := p.CNAB.FetchClaim("wordpress-mysql")
+	require.NoError(p.T(), err, "could not fetch claim for the dependency")
+	assert.Equal(p.T(), "ping", c.Result.Action, "the dependency wasn't recorded as being invoked")
+	assert.Equal(p.T(), claim.StatusSuccess, c.Result.Status, "the dependency wasn't recorded as being invoked successfully")
+
+	// Verify that the bundle claim is invoked
+	c, err = p.CNAB.FetchClaim("wordpress")
+	require.NoError(p.T(), err, "could not fetch claim for the root bundle")
+	assert.Equal(p.T(), "ping", c.Result.Action, "the root bundle wasn't recorded as being invoked")
+	assert.Equal(p.T(), claim.StatusSuccess, c.Result.Status, "the root bundle wasn't recorded as being invoked successfully")
+}
+
+func uninstallWordpressBundle(p *porter.TestPorter, namespace string) {
 	uninstallOptions := porter.UninstallOptions{}
 	uninstallOptions.CredentialIdentifiers = []string{"ci"}
 	uninstallOptions.Params = []string{ // See https://github.com/deislabs/porter/issues/474
@@ -168,16 +171,16 @@ func TestDependencies_Uninstall(t *testing.T) {
 		"mysql#mysql-name=porter-ci-mysql-" + namespace,
 	}
 	err := uninstallOptions.Validate([]string{}, p.Context)
-	require.NoError(t, err, "validation of uninstall opts for root bundle failed")
+	require.NoError(p.T(), err, "validation of uninstall opts for root bundle failed")
 
 	err = p.UninstallBundle(uninstallOptions)
-	require.NoError(t, err, "uninstall of root bundle failed")
+	require.NoError(p.T(), err, "uninstall of root bundle failed")
 
 	// Verify that the dependency claim is uninstalled
 	_, err = p.CNAB.FetchClaim("wordpress-mysql")
-	assert.EqualError(t, err, "could not retrieve claim wordpress-mysql: Claim does not exist")
+	assert.EqualError(p.T(), err, "could not retrieve claim wordpress-mysql: Claim does not exist")
 
 	// Verify that the bundle claim is uninstalled
 	_, err = p.CNAB.FetchClaim("wordpress")
-	assert.EqualError(t, err, "could not retrieve claim wordpress: Claim does not exist")
+	assert.EqualError(p.T(), err, "could not retrieve claim wordpress: Claim does not exist")
 }
