3adb74bf5f7404fd4f944d0a4e7a29bafaa02dde
Jeremy Rickard <jeremy.rickard@microsoft.com>
2019-08-04 19:26:15 -0600

    Update the workshop examples:
    * Add a bundle output to porter-tf and porter-tf-aci
    * Update the update/uninstall steps to not print anything
    * Updated porter-tf-aci to deploy a trivial webapp to ACI

--- workshop/porter-tf-aci/azure/app/main.go
+++ workshop/porter-tf-aci/azure/app/main.go
@@ -1,4 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	sqlFQDN := os.Getenv("MYSQL_FQDN")
+	if sqlFQDN == "" {
+		http.Error(w, "couldn't find MYSQL FQDN", http.StatusInternalServerError)
+	}
+
+	w.Write([]byte(fmt.Sprintf("Hello, I'm a webserver that wants to connect to a MYSQL at %s", sqlFQDN)))
+}
+
 func main() {
+
+	http.HandleFunc("/", handle)
+
+	fmt.Printf("Server starting on port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
