02ef226c9962bae7ff30aecaf375ec123c7cbbff
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2019-08-29 10:58:58 -0500

    Add reusable regex output processing
    A mixin can declare that it wants to scrape its output using a regular
    expression and all matches are put in a file, one per line.

---
+++ pkg/exec/builder/output_regex.go
@@ -0,0 +1,56 @@
+package builder
+
+import (
+	"bytes"
+	"regexp"
+	"strings"
+
+	"github.com/deislabs/porter/pkg/context"
+	"github.com/pkg/errors"
+)
+
+type OutputRegex interface {
+	Output
+	GetRegex() string
+}
+
+// ProcessJsonPathOutputs looks through the outputs for any that implement the OutputRegex,
+// applies the regular expression to the output buffer and extracts their output.
+func ProcessRegexOutputs(cxt *context.Context, step StepWithOutputs, outputB *bytes.Buffer) error {
+	outputs := step.GetOutputs()
+
+	if len(outputs) == 0 {
+		return nil
+	}
+
+	for _, o := range outputs {
+		output, ok := o.(OutputRegex)
+		if !ok {
+			continue
+		}
+
+		outputRegex := output.GetRegex()
+		outputName := output.GetName()
+
+		r, err := regexp.Compile(outputRegex)
+		if err != nil {
+			return errors.Wrapf(err, "invalid regular expression %q for output %q", outputRegex, outputName)
+		}
+
+		// Find every submatch / capture and put it on its own line in the output file
+		results := r.FindAllStringSubmatch(outputB.String(), -1)
+		var matches []string
+		for _, result := range results {
+			if len(result) > 1 { // Skip the first element which is the full match, we only want the capture groups
+				matches = append(matches, result[1:]...)
+			}
+		}
+		value := strings.Join(matches, "\n")
+		err = cxt.WriteMixinOutputToFile(outputName, []byte(value))
+		if err != nil {
+			return errors.Wrapf(err, "error writing mixin output for %q", outputName)
+		}
+	}
+
+	return nil
+}
