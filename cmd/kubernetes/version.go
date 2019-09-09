a53f571919780595cb2ad46797311eb16aeada39
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2019-09-09 15:14:58 -0500

    Use the veresion package in the k8s mixin

--- cmd/kubernetes/version.go
+++ cmd/kubernetes/version.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"github.com/deislabs/porter/pkg/kubernetes"
+	"github.com/deislabs/porter/pkg/porter/version"
 	"github.com/spf13/cobra"
 )
 
-func buildVersionCommand(mixin *kubernetes.Mixin) *cobra.Command {
-	return &cobra.Command{
+func buildVersionCommand(m *kubernetes.Mixin) *cobra.Command {
+	opts := version.Options{}
+
+	cmd := &cobra.Command{
 		Use:   "version",
 		Short: "Print the mixin verison",
-		Run: func(cmd *cobra.Command, args []string) {
-			mixin.PrintVersion()
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return opts.Validate()
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return m.PrintVersion(opts)
 		},
 	}
+
+	f := cmd.Flags()
+	f.StringVarP(&opts.RawFormat, "output", "o", string(version.DefaultVersionFormat),
+		"Specify an output format.  Allowed values: json, plaintext")
+
+	return cmd
 }
---
+++ pkg/kubernetes/version_test.go
@@ -0,0 +1,56 @@
+package kubernetes
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/deislabs/porter/pkg/printer"
+
+	"github.com/deislabs/porter/pkg/porter/version"
+	"github.com/stretchr/testify/require"
+
+	"github.com/deislabs/porter/pkg"
+)
+
+func TestPrintVersion(t *testing.T) {
+	pkg.Commit = "abc123"
+	pkg.Version = "v1.2.3"
+
+	m := NewTestMixin(t)
+
+	opts := version.Options{}
+	err := opts.Validate()
+	require.NoError(t, err)
+	m.PrintVersion(opts)
+
+	gotOutput := m.TestContext.GetOutput()
+	wantOutput := "kubernetes v1.2.3 (abc123) by DeisLabs"
+	if !strings.Contains(gotOutput, wantOutput) {
+		t.Fatalf("invalid output:\nWANT:\t%q\nGOT:\t%q\n", wantOutput, gotOutput)
+	}
+}
+
+func TestPrintJsonVersion(t *testing.T) {
+	pkg.Commit = "abc123"
+	pkg.Version = "v1.2.3"
+
+	m := NewTestMixin(t)
+
+	opts := version.Options{}
+	opts.RawFormat = string(printer.FormatJson)
+	err := opts.Validate()
+	require.NoError(t, err)
+	m.PrintVersion(opts)
+
+	gotOutput := m.TestContext.GetOutput()
+	wantOutput := `{
+  "name": "kubernetes",
+  "version": "v1.2.3",
+  "commit": "abc123",
+  "author": "DeisLabs"
+}
+`
+	if !strings.Contains(gotOutput, wantOutput) {
+		t.Fatalf("invalid output:\nWANT:\t%q\nGOT:\t%q\n", wantOutput, gotOutput)
+	}
+}
