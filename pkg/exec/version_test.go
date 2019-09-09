694ceddd7170941a7cc8660ffc85b45c25abf3b9
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2019-09-09 15:14:40 -0500

    Use the version package in the exec mixin

--- pkg/exec/version_test.go
+++ pkg/exec/version_test.go
@@ -4,6 +4,11 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/deislabs/porter/pkg/printer"
+
+	"github.com/deislabs/porter/pkg/porter/version"
+	"github.com/stretchr/testify/require"
+
 	"github.com/deislabs/porter/pkg"
 )
 
@@ -12,10 +17,39 @@ func TestPrintVersion(t *testing.T) {
 	pkg.Version = "v1.2.3"
 
 	m := NewTestMixin(t)
-	m.PrintVersion()
+
+	opts := version.Options{}
+	err := opts.Validate()
+	require.NoError(t, err)
+	m.PrintVersion(opts)
 
 	gotOutput := m.TestContext.GetOutput()
-	wantOutput := "exec mixin v1.2.3 (abc123)"
+	wantOutput := "exec v1.2.3 (abc123) by DeisLabs"
+	if !strings.Contains(gotOutput, wantOutput) {
+		t.Fatalf("invalid output:\nWANT:\t%q\nGOT:\t%q\n", wantOutput, gotOutput)
+	}
+}
+
+func TestPrintJsonVersion(t *testing.T) {
+	pkg.Commit = "abc123"
+	pkg.Version = "v1.2.3"
+
+	m := NewTestMixin(t)
+
+	opts := version.Options{}
+	opts.RawFormat = string(printer.FormatJson)
+	err := opts.Validate()
+	require.NoError(t, err)
+	m.PrintVersion(opts)
+
+	gotOutput := m.TestContext.GetOutput()
+	wantOutput := `{
+  "name": "exec",
+  "version": "v1.2.3",
+  "commit": "abc123",
+  "author": "DeisLabs"
+}
+`
 	if !strings.Contains(gotOutput, wantOutput) {
 		t.Fatalf("invalid output:\nWANT:\t%q\nGOT:\t%q\n", wantOutput, gotOutput)
 	}
