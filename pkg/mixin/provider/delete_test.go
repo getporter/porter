cee6ef784e0ccbc8204e7715444a5639211c3a44
Brad Cypert <brad.cypert@gmail.com>
2019-09-19 14:26:58 -0400

    Add in test

--- pkg/mixin/provider/delete_test.go
+++ pkg/mixin/provider/delete_test.go
@@ -1,1 +1,29 @@
 package mixinprovider
+
+import (
+	"path"
+	"testing"
+
+	"github.com/deislabs/porter/pkg/config"
+	"github.com/deislabs/porter/pkg/mixin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFileSystem_Delete_DeleteMixin(t *testing.T) {
+	c := config.NewTestConfig(t)
+	p := NewFileSystem(c.Config)
+
+	mixinsDir, _ := p.GetMixinsDir()
+	mixinDir := path.Join(mixinsDir, "mixxin")
+
+	opts := mixin.DeleteOptions{
+		Name: "mixxin",
+	}
+	opts.Validate([]string{"mixxin"})
+
+	p.Delete(opts)
+
+	// Make sure the mixin directory was removed
+	mixinDirExists, _ := p.FileSystem.DirExists(mixinDir)
+	assert.False(t, mixinDirExists)
+}
