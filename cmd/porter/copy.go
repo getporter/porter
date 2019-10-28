ed24c7b55e3a3b74dd019afe247a6ab40929cddf
Jeremy Rickard <jeremy.rickard@microsoft.com>
2019-10-28 12:24:22 -0600

    Fix whitespace in the copy help text
    Signed-off-by: Jeremy Rickard <jeremy.rickard@microsoft.com>

--- cmd/porter/copy.go
+++ cmd/porter/copy.go
@@ -13,13 +13,11 @@ func buildBundleCopyCommand(p *porter.Porter) *cobra.Command {
 	cmd := cobra.Command{
 		Use:   "copy",
 		Short: "Copy a bundle",
-		Long: `Copy a published bundle from one registry to another.
-		
-		Source bundle can be either a tagged reference or a digest reference.
-
-		Destination can be either a registry, a registry/repository, or a fully tagged bundle reference. 
-		If the source bundle is a digest reference, destination must be a tagged reference.
-		`,
+		Long: `Copy a published bundle from one registry to another.		
+Source bundle can be either a tagged reference or a digest reference.
+Destination can be either a registry, a registry/repository, or a fully tagged bundle reference. 
+If the source bundle is a digest reference, destination must be a tagged reference.
+`,
 		Example: `  porter bundle copy
   porter bundle copy --source deislabs/porter-bundle:v0.1.0 --destination portersh
   porter bundle copy --source deislabs/porter-bundle:v0.1.0 --destination portersh --insecure-registry
