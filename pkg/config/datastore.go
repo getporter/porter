d5b710715a1eac6e143a2827fd3f495afd8b3c5c
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2019-10-29 16:57:47 -0500

    Pass plugin config via stdin (#776)
    * Use a patch of hashicorp/go-plugin
    
    https://github.com/hashicorp/go-plugin/pull/127
    
    * Sync vendor
    
    * Pass plugin config via stdin

--- pkg/config/datastore.go
+++ pkg/config/datastore.go
@@ -1,10 +1,20 @@
 package config
 
+import "errors"
+
 // Data is the data stored in PORTER_HOME/porter.toml|yaml|json
 type Data struct {
 	// Only define fields here that you need to access from code
 	// Values are dynamically applied to flags and don't need to be defined
-	InstanceStoragePlugin string `mapstructure:"instance-storage-plugin"`
+	InstanceStoragePlugin string          `mapstructure:"instance-storage-plugin"`
+	DefaultInstanceStore  string          `mapstructure:"default-instance-store"`
+	InstanceStores        []InstanceStore `mapstructure:"instance-store"`
+}
+
+type InstanceStore struct {
+	Name         string                 `mapstructure:"name"`
+	PluginSubkey string                 `mapstructure:"plugin"`
+	Config       map[string]interface{} `mapstructure:"config"`
 }
 
 func (d *Data) GetInstanceStoragePlugin() string {
@@ -13,6 +23,26 @@ func (d *Data) GetInstanceStoragePlugin() string {
 	}
 
 	return d.InstanceStoragePlugin
+}
+
+func (d *Data) GetDefaultInstanceStore() string {
+	if d == nil {
+		return ""
+	}
+
+	return d.DefaultInstanceStore
+}
+
+func (d *Data) GetInstanceStore(name string) (InstanceStore, error) {
+	if d != nil {
+		for _, is := range d.InstanceStores {
+			if is.Name == name {
+				return is, nil
+			}
+		}
+	}
+
+	return InstanceStore{}, errors.New("instance-store %q not defined")
 }
 
 var _ DataStoreLoaderFunc = NoopDataLoader
--- pkg/plugins/plugins.go
+++ pkg/plugins/plugins.go
@@ -1,6 +1,10 @@
 package plugins
 
 import (
+	"errors"
+	"fmt"
+	"strings"
+
 	"github.com/hashicorp/go-plugin"
 )
 
@@ -9,4 +13,39 @@ var HandshakeConfig = plugin.HandshakeConfig{
 	ProtocolVersion:  1,
 	MagicCookieKey:   "PORTER",
 	MagicCookieValue: "bbc2dd71-def4-4311-906e-e98dc27208ce",
+}
+
+type PluginKey struct {
+	Binary         string
+	Interface      string
+	Implementation string
+	IsInternal     bool
+}
+
+func (k PluginKey) String() string {
+	return fmt.Sprintf("%s.%s.%s", k.Interface, k.Binary, k.Implementation)
+}
+
+func ParsePluginKey(value string) (PluginKey, error) {
+	var key PluginKey
+
+	parts := strings.Split(value, ".")
+
+	switch len(parts) {
+	case 1:
+		key.IsInternal = true
+		key.Binary = "porter"
+		key.Implementation = parts[0]
+	case 2:
+		key.Binary = parts[0]
+		key.Implementation = parts[1]
+	case 3:
+		key.Interface = parts[0]
+		key.Binary = parts[1]
+		key.Implementation = parts[2]
+	default:
+		return PluginKey{}, errors.New("invalid plugin key %q, allowed format is [INTERFACE].BINARY.IMPLEMENTATION")
+	}
+
+	return key, nil
 }
