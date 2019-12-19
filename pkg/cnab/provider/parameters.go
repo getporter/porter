b61bbe7147f25d1cc637327916046dee499fd8fc
Vaughn Dice <vadice@microsoft.com>
2019-12-19 13:14:31 -0700

    reduce parameter matrix test to one test in parameters_test.go
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>

--- pkg/cnab/provider/parameters.go
+++ pkg/cnab/provider/parameters.go
@@ -46,10 +46,10 @@ func (d *Runtime) loadParameters(claim *claim.Claim, rawOverrides map[string]str
 		}
 	}
 
-	// rawOverrides may supply no entry for a parameter designated as required
-	// *but* that does not apply to this action.
+	// rawOverrides (meaning, user-supplied overrides at time of action invocation)
+	// may supply no entry for a parameter designated as required *but* that does not apply to this action.
 	//
-	// When this occurs, we set an override to either the default value or the current value in the claim.
+	// When this occurs, we set an override to either the current value in the claim or the default value.
 	// If neither exists, the zero value according to the parameter type will be used.
 	// Otherwise, if unset/nil, json validation in bundle.ValuesOrDefaults would return an error
 	for name, param := range bun.Parameters {
@@ -57,13 +57,19 @@ func (d *Runtime) loadParameters(claim *claim.Claim, rawOverrides map[string]str
 		if !ok {
 			return nil, fmt.Errorf("definition %s not defined in bundle", param.Definition)
 		}
-
 		if param.Required {
 			if _, exists := rawOverrides[name]; !exists {
-				if def.Default != nil {
-					overrides[name] = def.Default
-				} else if !param.AppliesTo(action) {
-					overrides[name] = getClaimValueOrZeroValue(name, def, claim)
+				if !param.AppliesTo(action) {
+					// First defer to a pre-existing value in the claim
+					if claim.Parameters[name] != nil {
+						overrides[name] = claim.Parameters[name]
+					} else if def.Default != nil {
+						// Next defer to a default value
+						overrides[name] = def.Default
+					} else {
+						// Finally, use a zero value if no other option exists
+						overrides[name] = getZeroValue(name, def, claim)
+					}
 				}
 			}
 		}
@@ -86,12 +92,8 @@ func (d *Runtime) getUnconvertedValueFromRaw(def *definition.Schema, key, rawVal
 	return rawValue, nil
 }
 
-// getClaimValueOrZeroValue returns a
-func getClaimValueOrZeroValue(name string, def *definition.Schema, claim *claim.Claim) interface{} {
-	if claim.Parameters[name] != nil {
-		return claim.Parameters[name]
-	}
-
+// getZeroValue returns the zero value for a parameter according to its type
+func getZeroValue(name string, def *definition.Schema, claim *claim.Claim) interface{} {
 	switch def.Type {
 	case "integer", "number":
 		return 0
--- pkg/cnab/provider/parameters_test.go
+++ pkg/cnab/provider/parameters_test.go
@@ -1,6 +1,8 @@
 package cnabprovider
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"testing"
 
 	"get.porter.sh/porter/pkg/config"
@@ -188,10 +190,10 @@ func Test_loadParameters_requiredButDoesNotApply(t *testing.T) {
 	params, err := d.loadParameters(claim, overrides, "action")
 	require.NoError(t, err)
 
-	require.Equal(t, "foo-claim-value", params["foo"], "expected param 'foo' to be the bundle default")
+	require.Equal(t, "foo-claim-value", params["foo"], "expected param 'foo' to be the previous value from the claim")
 }
 
-func Test_loadParameters_requiredButdoesNotApply_claimValueNonExistent(t *testing.T) {
+func Test_loadParameters_zeroValues(t *testing.T) {
 	var emptyStruct struct{}
 
 	testcases := []struct {
@@ -279,4 +281,182 @@ func Test_loadParameters_fileParameter(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Equal(t, "SGVsbG8gV29ybGQh", params["foo"], "expected param 'foo' to be the base64-encoded file contents")
+}
+
+// This is intended to cover the matrix of cases around parameter value resolution.
+// It exercises the matrix for all supported actions.
+func Test_Paramapalooza(t *testing.T) {
+	actions := []string{"install", "upgrade", "uninstall", "invoke"}
+	for _, action := range actions {
+		t.Run(action, func(t *testing.T) {
+			testcases := []struct {
+				name            string
+				required        bool
+				provided        bool
+				defaultExists   bool
+				appliesToAction bool
+				expectedVal     interface{}
+				expectedErr     string
+			}{
+				// Are you ready to enter the Matrix?
+				{"required, provided, default exists, applies to action",
+					true, true, true, true, "my-param-value", "",
+				},
+				{"required, provided, default exists, does not apply to action",
+					true, true, true, false, "my-param-value", "",
+				},
+				{"required, provided, default does not exist, applies to action",
+					true, true, false, true, "my-param-value", "",
+				},
+				{"required, provided, default does not exist, does not apply to action",
+					true, true, false, false, "my-param-value", "",
+				},
+				// As of writing, bundle.ValuesOrDefaults in cnab-go requires a specific override
+				// be provided if applicable to an action.
+				// Otherwise, it errors out and does not look up/use default in this case.
+				{"required, not provided, default exists, applies to action",
+					true, false, true, true, nil, "invalid parameters: parameter \"my-param\" is required",
+				},
+				{"required, not provided, default exists, does not apply to action",
+					true, false, true, false, "my-param-default", "",
+				},
+				{"required, not provided, default does not exist, applies to action",
+					true, false, false, true, nil, "invalid parameters: parameter \"my-param\" is required",
+				},
+				{"required, not provided, default does not exist, does not apply to action",
+					true, false, false, false, "", "",
+				},
+				{"not required, provided, default exists, applies to action",
+					false, true, true, true, "my-param-value", "",
+				},
+				{"not required, provided, default exists, does not apply to action",
+					false, true, true, false, "my-param-value", "",
+				},
+				{"not required, provided, default does not exist, applies to action",
+					false, true, false, true, "my-param-value", "",
+				},
+				{"not required, provided, default does not exist, does not apply to action",
+					false, true, false, false, "my-param-value", "",
+				},
+				{"not required, not provided, default exists, applies to action",
+					false, false, true, true, "my-param-default", "",
+				},
+				{"not required, not provided, default exists, does not apply to action",
+					false, false, true, false, "my-param-default", "",
+				},
+				{"not required, not provided, default does not exist, applies to action",
+					false, false, false, true, nil, "",
+				},
+				{"not required, not provided, default does not exist, does not apply to action",
+					false, false, false, false, nil, "",
+				},
+			}
+
+			for _, tc := range testcases {
+				t.Run(tc.name, func(t *testing.T) {
+					c := config.NewTestConfig(t)
+					instanceStorage := instancestorage.NewTestInstanceStorageProvider()
+					d := NewRuntime(c.Config, instanceStorage)
+
+					bun := &bundle.Bundle{
+						Name:          "mybuns",
+						Version:       "v1.0.0",
+						SchemaVersion: "v1.0.0",
+						Actions: map[string]bundle.Action{
+							"zombies": {
+								Modifies: true,
+							},
+						},
+						InvocationImages: []bundle.InvocationImage{
+							{
+								BaseImage: bundle.BaseImage{
+									Image:     "mybuns:latest",
+									ImageType: "docker",
+								},
+							},
+						},
+						Definitions: definition.Definitions{
+							"my-param": &definition.Schema{
+								Type: "string",
+							},
+						},
+						Parameters: map[string]bundle.Parameter{
+							"my-param": bundle.Parameter{
+								Definition: "my-param",
+								Required:   tc.required,
+							},
+						},
+					}
+
+					if tc.defaultExists {
+						bun.Definitions["my-param"].Default = "my-param-default"
+					}
+
+					if !tc.appliesToAction {
+						param := bun.Parameters["my-param"]
+						param.ApplyTo = []string{"non-applicable-action"}
+						bun.Parameters["my-param"] = param
+					}
+
+					args := ActionArguments{
+						Claim:    "test",
+						Insecure: true,
+						Driver:   "debug",
+					}
+					// If param is provided (via --param/--param-file)
+					// it will be attached to args
+					if tc.provided {
+						args.Params = map[string]string{
+							"my-param": "my-param-value",
+						}
+					}
+
+					// If action is install, no claim is expected to exist
+					// so we write a bundle and pull it in via ActionArguments
+					if action == "install" {
+						bytes, err := json.Marshal(bun)
+						require.NoError(t, err)
+
+						// We currently need to read/write from the same file on disk
+						// as cnab-go's bundle loader still makes raw os calls for loading a bundle
+						err = ioutil.WriteFile("testdata/bundle.json", bytes, 0644)
+						require.NoError(t, err)
+
+						args.BundlePath = "testdata/bundle.json"
+					} else {
+						// For all other actions, a claim is expected to exist
+						// so we create one here and add the bundle to the claim
+						claim, err := claim.New("test")
+						require.NoError(t, err)
+
+						claim.Bundle = bun
+						d.instanceStorage.Store(*claim)
+					}
+
+					var err error
+					switch action {
+					case "install":
+						err = d.Install(args)
+					case "upgrade":
+						err = d.Upgrade(args)
+					case "invoke":
+						err = d.Invoke("zombies", args)
+					case "uninstall":
+						err = d.Uninstall(args)
+					}
+
+					if tc.expectedErr != "" {
+						require.EqualError(t, err, tc.expectedErr)
+					} else if action != "uninstall" {
+						require.NoError(t, err)
+
+						// Verify the updated param value on the generated claim
+						updatedClaim, err := d.instanceStorage.Read("test")
+						require.NoError(t, err)
+						require.Equal(t, tc.expectedVal, updatedClaim.Parameters["my-param"])
+					}
+				})
+			}
+		})
+	}
 }
