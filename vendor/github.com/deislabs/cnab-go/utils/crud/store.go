7bc9e2f2beaf1ba7338cc1652ecf4e848d7f7ed8
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2020-01-03 15:30:41 -0600

    Remove vendor directory
    In a separate PR we should add in the Athens proxy

--- vendor/github.com/deislabs/cnab-go/utils/crud/store.go
+++
@@ -1,9 +0,0 @@
-package crud
-
-// Store is a simplified interface to a key-blob store supporting CRUD operations.
-type Store interface {
-	List() ([]string, error)
-	Store(name string, data []byte) error
-	Read(name string) ([]byte, error)
-	Delete(name string) error
-}
--- vendor/github.com/docker/cnab-to-oci/relocation/types.go
+++
@@ -1,4 +0,0 @@
-package relocation
-
-// ImageRelocationMap stores the mapping between the original image reference as key, and the relocated reference as a value.
-type ImageRelocationMap map[string]string
