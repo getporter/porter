5dfc278d9b3236c2c985e5527658c1dd1eee8dc1
REDDY PRASAD <dev.drprasad@aim.com>
2020-05-26 22:22:20 +0530

    🚚 Remove unit test and add it as integration test

--- pkg/plugins/pluggable/loader_test.go
+++ pkg/plugins/pluggable/loader_test.go
@@ -1,12 +1,10 @@
 package pluggable
 
 import (
-	"os/exec"
 	"testing"
 
 	"get.porter.sh/porter/pkg/config"
 	"get.porter.sh/porter/pkg/plugins"
-	"get.porter.sh/porter/pkg/secrets"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -37,43 +35,6 @@ func TestPluginLoader_SelectPlugin(t *testing.T) {
 
 		assert.Equal(t, &plugins.PluginKey{Binary: "porter", Implementation: "filesystem", IsInternal: true}, l.SelectedPluginKey)
 		assert.Nil(t, l.SelectedPluginConfig)
-	})
-
-	t.Run("plugin throws an error", func(t *testing.T) {
-		porterPath, _ := c.GetHomeDir()
-		exec.Command("mkdir", porterPath+"/plugins/badplugin/").Run()
-		exec.Command("cp", "testdata/badplugin", porterPath+"/plugins/badplugin/").Run()
-		defer exec.Command("rm", "-rf", porterPath+"/plugins/badplugin").Run()
-
-		cfg := PluginTypeConfig{
-			Interface: secrets.PluginInterface,
-			Plugin:    &secrets.Plugin{},
-			GetDefaultPluggable: func(datastore *config.Data) string {
-				return datastore.GetDefaultSecretSource()
-			},
-			GetPluggable: func(datastore *config.Data, name string) (Entry, error) {
-				return datastore.GetSecretSource(name)
-			},
-			GetDefaultPlugin: func(datastore *config.Data) string {
-				return datastore.GetDefaultSecretsPlugin()
-			},
-		}
-		c.Data = &config.Data{
-			DefaultSecrets: "myplugin",
-			SecretSources: []config.SecretSource{{
-				PluginConfig: config.PluginConfig{
-					Name:         "myplugin",
-					PluginSubKey: "badplugin.vault",
-				},
-			}},
-		}
-		ll := NewPluginLoader(c.Config)
-		_, _, err := ll.Load(cfg)
-		require.EqualError(t, err, `could not connect to the secrets.badplugin.vault plugin: Unrecognized remote plugin message: 
-
-This usually means that the plugin is either invalid or simply
-needs to be recompiled to support the latest protocol.: invalid plugin key
-`)
 	})
 
 	t.Run("external plugin", func(t *testing.T) {
---
+++ pkg/plugins/pluggable/plugins_integration_test.go
@@ -0,0 +1,58 @@
+// +build integration
+
+package pluggable
+
+import (
+	"os/exec"
+	"path"
+	"testing"
+
+	"get.porter.sh/porter/pkg/config"
+	"get.porter.sh/porter/pkg/secrets"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPlugins_CatchStderr(t *testing.T) {
+	c := config.NewTestConfig(t)
+	c.NewCommand = exec.Command
+
+	t.Run("plugin throws an error", func(t *testing.T) {
+		pluginsPath, _ := c.GetPluginsDir()
+		pluginName := "badplugin"
+
+		err := c.FileSystem.MkdirAll(path.Join(pluginsPath, pluginName), 0777)
+		require.NoError(t, err, "could not create plugin dir")
+		err = exec.Command("cp", path.Join("testdata", pluginName), path.Join(pluginsPath, pluginName)).Run()
+		require.NoError(t, err, "could not copy test binary")
+
+		cfg := PluginTypeConfig{
+			Interface: secrets.PluginInterface,
+			Plugin:    &secrets.Plugin{},
+			GetDefaultPluggable: func(datastore *config.Data) string {
+				return datastore.GetDefaultSecretSource()
+			},
+			GetPluggable: func(datastore *config.Data, name string) (Entry, error) {
+				return datastore.GetSecretSource(name)
+			},
+			GetDefaultPlugin: func(datastore *config.Data) string {
+				return datastore.GetDefaultSecretsPlugin()
+			},
+		}
+		c.Data = &config.Data{
+			DefaultSecrets: "myplugin",
+			SecretSources: []config.SecretSource{{
+				PluginConfig: config.PluginConfig{
+					Name:         "myplugin",
+					PluginSubKey: "badplugin.vault",
+				},
+			}},
+		}
+		ll := NewPluginLoader(c.Config)
+		_, _, err = ll.Load(cfg)
+		require.EqualError(t, err, `could not connect to the secrets.badplugin.vault plugin: Unrecognized remote plugin message: 
+
+This usually means that the plugin is either invalid or simply
+needs to be recompiled to support the latest protocol.: invalid plugin key
+`)
+	})
+}
