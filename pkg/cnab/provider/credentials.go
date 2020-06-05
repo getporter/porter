d64b24502399f3aff1d16ba08efda0f18a557c20
Carolyn Van Slyck <carolyn.vanslyck@microsoft.com>
2020-06-05 11:35:37 -0500

    Fix specifying credentials by path
    When credentials were refactored to support secret stores, there was a
    regression around loading credentials by path. This allowed people to
    supply a one-off credential set when running a bundle and has now been
    corrected.
    
    e.g. porter install -c ./mycreds.json

--- pkg/cnab/provider/credentials.go
+++ pkg/cnab/provider/credentials.go
@@ -1,9 +1,11 @@
 package cnabprovider
 
 import (
+	"encoding/json"
 	"path/filepath"
 	"strings"
 
+	"github.com/pkg/errors"
 	"github.com/cnabio/cnab-go/bundle"
 	"github.com/cnabio/cnab-go/credentials"
 )
@@ -18,7 +20,13 @@ func (d *Runtime) loadCredentials(b *bundle.Bundle, creds []string) (credentials
 	// in which they were supplied on the CLI.
 	resolvedCredentials := credentials.Set{}
 	for _, name := range creds {
-		cset, err := d.credentials.Read(name)
+		var cset credentials.CredentialSet
+		var err error
+		if d.isPathy(name) {
+			cset, err = d.loadCredentialFromFile(name)
+		} else {
+			cset, err = d.credentials.Read(name)
+		}
 		if err != nil {
 			return nil, err
 		}
@@ -40,4 +48,15 @@ func (d *Runtime) isPathy(name string) bool {
 	// TODO: export back outta Compton
 
 	return strings.Contains(name, string(filepath.Separator))
+}
+
+func (d *Runtime) loadCredentialFromFile(path string) (credentials.CredentialSet, error) {
+	data, err := d.FileSystem.ReadFile(path)
+	if err != nil {
+		return credentials.CredentialSet{}, errors.Wrapf(err, "could not read file %s", path)
+	}
+
+	var cs credentials.CredentialSet
+	err = json.Unmarshal(data, &cs)
+	return cs, errors.Wrapf(err, "error loading credential set in %s", path)
 }
