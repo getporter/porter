6a4de4cc324b1a9a75349df30cf0c8dffb6c3a2a
Vaughn Dice <vadice@microsoft.com>
2020-06-05 15:30:23 -0600

    add unit tests for parameterStorage and parameterstore
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>

---
+++ pkg/parameters/parameterStorage_test.go
@@ -0,0 +1,155 @@
+package parameters
+
+import (
+	"testing"
+
+	"get.porter.sh/porter/pkg/config"
+	"get.porter.sh/porter/pkg/secrets"
+	inmemorysecrets "get.porter.sh/porter/pkg/secrets/in-memory"
+	inmemorystorage "get.porter.sh/porter/pkg/storage/in-memory"
+	"github.com/cnabio/cnab-go/valuesource"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParameterStorage_ResolveAll(t *testing.T) {
+	// The inmemory secret store currently only supports secret sources
+	// So all of these have this same source
+	testParameterSet := ParameterSet{
+		Name: "myparamset",
+		Parameters: []valuesource.Strategy{
+			{
+				Name: "param1",
+				Source: valuesource.Source{
+					Key:   "secret",
+					Value: "param1",
+				},
+			},
+			{
+				Name: "param2",
+				Source: valuesource.Source{
+					Key:   "secret",
+					Value: "param2",
+				},
+			},
+		},
+	}
+
+	t.Run("resolve params success", func(t *testing.T){
+		tc := config.NewTestConfig(t)
+		backingSecrets := inmemorysecrets.NewStore()
+		backingParams := inmemorystorage.NewStore()
+		paramStore := NewParameterStore(backingParams)
+		secretStore := secrets.NewSecretStore(backingSecrets)
+	
+		parameterStorage := ParameterStorage{
+			Config:          tc.Config,
+			ParametersStore: &paramStore,
+			SecretsStore:    secretStore,
+		}
+
+		backingSecrets.AddSecret("param1", "param1_value")
+		backingSecrets.AddSecret("param2", "param2_value")
+
+		expected := valuesource.Set{
+			"param1":"param1_value",
+			"param2":"param2_value",
+		}
+
+		resolved, err := parameterStorage.ResolveAll(testParameterSet)
+		require.NoError(t, err)
+		require.Equal(t, expected, resolved)
+	})
+
+	t.Run("resolve params failure", func(t *testing.T){
+		tc := config.NewTestConfig(t)
+		backingSecrets := inmemorysecrets.NewStore()
+		backingParams := inmemorystorage.NewStore()
+		paramStore := NewParameterStore(backingParams)
+		secretStore := secrets.NewSecretStore(backingSecrets)
+	
+		parameterStorage := ParameterStorage{
+			Config:          tc.Config,
+			ParametersStore: &paramStore,
+			SecretsStore:    secretStore,
+		}
+
+		// Purposefully only adding one secret
+		backingSecrets.AddSecret("param1", "param1_value")
+
+		expected := valuesource.Set{
+			"param1":"param1_value",
+			"param2":"",
+		}
+
+		resolved, err := parameterStorage.ResolveAll(testParameterSet)
+		require.EqualError(t, err, "1 error occurred:\n\t* unable to resolve parameter myparamset.param2 from secret param2: secret not found\n\n")
+		require.Equal(t, expected, resolved)
+	})
+}
+
+func TestParameterStorage_Validate(t *testing.T) {
+	t.Run("valid sources", func(t *testing.T) {
+		s := ParameterStorage{}
+
+		testParameterSet := ParameterSet{
+			Parameters: []valuesource.Strategy{
+				{
+					Source: valuesource.Source{
+						Key:   "env",
+						Value: "SOME_ENV",
+					},
+				},
+				{
+					Source: valuesource.Source{
+						Key:   "value",
+						Value: "somevalue",
+					},
+				},
+				{
+					Source: valuesource.Source{
+						Key:   "path",
+						Value: "/some/path",
+					},
+				},
+				{
+					Source: valuesource.Source{
+						Key:   "command",
+						Value: "some command",
+					},
+				},
+				{
+					Source: valuesource.Source{
+						Key:   "secret",
+						Value: "secret",
+					},
+				},
+			},
+		}
+	
+		err := s.Validate(testParameterSet)
+		require.NoError(t, err, "Validate did not return errors")
+	})
+
+	t.Run("invalid sources", func(t *testing.T){
+		s := ParameterStorage{}
+		testParameterSet := ParameterSet{
+			Parameters: []valuesource.Strategy{
+				{
+					Source: valuesource.Source{
+						Key:   "wrongthing",
+						Value: "SOME_ENV",
+					},
+				},
+				{
+					Source: valuesource.Source{
+						Key:   "anotherwrongthing",
+						Value: "somevalue",
+					},
+				},
+			},
+		}
+	
+		err := s.Validate(testParameterSet)
+		require.Error(t, err, "Validate returned errors")
+	})
+}
---
+++ pkg/parameters/parameterstore_test.go
@@ -0,0 +1,55 @@
+package parameters
+
+import (
+	"testing"
+
+	inmemorystorage "get.porter.sh/porter/pkg/storage/in-memory"
+	"github.com/cnabio/cnab-go/valuesource"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewParameterStore(t *testing.T) {
+	backingParams := inmemorystorage.NewStore()
+	paramStore := NewParameterStore(backingParams)
+
+	params, err := paramStore.List()
+	require.NoError(t, err)
+	require.Equal(t, []string{}, params, "List should return no entries")
+
+	myParamSet := ParameterSet{
+		Name: "myparams",
+		Parameters: []valuesource.Strategy{
+			{
+				Name: "myparam",
+				Source: valuesource.Source{
+					Key: "value",
+					Value: "myparamvalue",
+				},
+			},
+		},
+	}
+	err = paramStore.Save(myParamSet)
+	require.NoError(t, err, "Save should successfully save")
+
+	params, err = paramStore.List()
+	require.NoError(t, err)
+	require.Equal(t, []string{"myparams"}, params, "List should return the saved entry")
+
+	pset, err := paramStore.Read("myparams")
+	require.NoError(t, err)
+	require.Equal(t, myParamSet, pset, "Read should return the saved parameter set")
+
+	psets, err := paramStore.ReadAll()
+	require.NoError(t, err)
+	require.Equal(t, []ParameterSet{myParamSet}, psets, "ReadAll should return all parameter sets")
+
+	err = paramStore.Delete("myparams")
+	require.NoError(t, err, "Delete should successfully delete the parameter set")
+
+	params, err = paramStore.List()
+	require.NoError(t, err)
+	require.Equal(t, []string{}, params, "List should return no entries")
+
+	pset, err = paramStore.Read("myparams")
+	require.EqualError(t, err, "Parameter set does not exist", "Read should return an error")
+}
