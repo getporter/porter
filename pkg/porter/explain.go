251a920201544c48a5167e2ad5ae7c8adbaa3489
REDDY PRASAD <dev.drprasad@aim.com>
2020-06-29 23:24:24 +0530

    👌🏼 Make changes as per feedback

--- pkg/porter/explain.go
+++ pkg/porter/explain.go
@@ -114,7 +114,8 @@ func (s SortPrintableAction) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
 }
 
-var porterInternalParams = []string{"porter-debug"}
+// Using empty structs instead of booleans cuz empty structs occupy zero memory
+var porterInternalParams = map[string]struct{}{"porter-debug": {}}
 
 func (o *ExplainOpts) Validate(args []string, cxt *context.Context) error {
 	err := o.sharedOptions.Validate(args, cxt)
@@ -171,15 +172,9 @@ func (p *Porter) printBundleExplain(o ExplainOpts, pb *PrintableBundle) error {
 	}
 }
 
-func checkIsInternalParam(inputParam string) bool {
-	isInternalParam := false
-	for _, internalParam := range porterInternalParams {
-		if inputParam == internalParam {
-			isInternalParam = true
-			break
-		}
-	}
-	return isInternalParam
+func isInternalParam(inputParam string) bool {
+	_, ok := porterInternalParams[inputParam]
+	return ok
 }
 
 func generatePrintable(bun *bundle.Bundle) (*PrintableBundle, error) {
@@ -216,7 +211,7 @@ func generatePrintable(bun *bundle.Bundle) (*PrintableBundle, error) {
 
 	params := []PrintableParameter{}
 	for p, v := range bun.Parameters {
-		if checkIsInternalParam(p) {
+		if isInternalParam(p) {
 			continue
 		}
 		def, ok := bun.Definitions[v.Definition]
