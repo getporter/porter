488a832173f73a15cf3ef5c7a47caa1d6f0df1a7
Carolyn Van Slyck <me@carolynvanslyck.com>
2020-11-16 20:09:07 -0600

    Recategorize test
    TestInstallFromTag_ManageFromClaim hits docker and should be
    an integration test
    
    Signed-off-by: Carolyn Van Slyck <me@carolynvanslyck.com>

---
+++ pkg/porter/lifecycle_integration_test.go
@@ -0,0 +1,38 @@
+// +build integration
+
+package porter
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestInstallFromTag_ManageFromClaim(t *testing.T) {
+	p := NewTestPorter(t)
+	cacheDir, _ := p.Cache.GetCacheDir()
+	p.TestConfig.TestContext.AddTestDirectory("testdata/cache", cacheDir)
+
+	installOpts := NewInstallOptions()
+	installOpts.Name = "hello"
+	installOpts.Tag = "getporter/porter-hello:v0.1.1"
+	err := installOpts.Validate(nil, p.Porter)
+	require.NoError(t, err, "InstallOptions.Validate failed")
+
+	err = p.InstallBundle(installOpts)
+	require.NoError(t, err, "InstallBundle failed")
+
+	upgradeOpts := NewUpgradeOptions()
+	upgradeOpts.Name = installOpts.Name
+	err = upgradeOpts.Validate(nil, p.Porter)
+
+	err = p.UpgradeBundle(upgradeOpts)
+	require.NoError(t, err, "UpgradeBundle failed")
+
+	uninstallOpts := NewUninstallOptions()
+	uninstallOpts.Name = installOpts.Name
+	err = uninstallOpts.Validate(nil, p.Porter)
+
+	err = p.UninstallBundle(uninstallOpts)
+	require.NoError(t, err, "UninstallBundle failed")
+}
--- pkg/porter/lifecycle_test.go
+++ pkg/porter/lifecycle_test.go
@@ -185,32 +185,3 @@ func TestManifestIgnoredWithTag(t *testing.T) {
 		require.NoError(t, err, "Validate failed")
 	})
 }
-
-func TestInstallFromTag_ManageFromClaim(t *testing.T) {
-	p := NewTestPorter(t)
-	cacheDir, _ := p.Cache.GetCacheDir()
-	p.TestConfig.TestContext.AddTestDirectory("testdata/cache", cacheDir)
-
-	installOpts := NewInstallOptions()
-	installOpts.Name = "hello"
-	installOpts.Tag = "getporter/porter-hello:v0.1.1"
-	err := installOpts.Validate(nil, p.Porter)
-	require.NoError(t, err, "InstallOptions.Validate failed")
-
-	err = p.InstallBundle(installOpts)
-	require.NoError(t, err, "InstallBundle failed")
-
-	upgradeOpts := NewUpgradeOptions()
-	upgradeOpts.Name = installOpts.Name
-	err = upgradeOpts.Validate(nil, p.Porter)
-
-	err = p.UpgradeBundle(upgradeOpts)
-	require.NoError(t, err, "UpgradeBundle failed")
-
-	uninstallOpts := NewUninstallOptions()
-	uninstallOpts.Name = installOpts.Name
-	err = uninstallOpts.Validate(nil, p.Porter)
-
-	err = p.UninstallBundle(uninstallOpts)
-	require.NoError(t, err, "UninstallBundle failed")
-}
