ecc5f6cf0f0ddc2a89b5c17538d5dd1806f938ab
Carolyn Van Slyck <me@carolynvanslyck.com>
2020-12-23 10:54:32 -0600

    Bump helm to supported version in our workshop image
    * Helm versions lower than v2.17.0 point to the deprecated helm repositories.
    * Validate docker images during PR builds
    
    Signed-off-by: Carolyn Van Slyck <me@carolynvanslyck.com>

---
+++ scripts/build-images.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+
+# PERMALINK and VERSION must be set before calling this script
+# It is intended to only be executed by make publish
+
+if [[ "$PERMALINK" == "latest" ]]; then
+  docker build --build-arg PERMALINK=$VERSION -t getporter/porter:$VERSION build/images/client
+  docker build --build-arg PERMALINK=$VERSION -t getporter/workshop:$VERSION build/images/workshop
+
+  docker tag getporter/porter:$VERSION getporter/porter:$PERMALINK
+  docker tag getporter/workshop:$VERSION getporter/workshop:$PERMALINK
+else
+  docker build --build-arg PERMALINK=$PERMALINK -t getporter/porter:$PERMALINK build/images/client
+  docker build --build-arg PERMALINK=$PERMALINK -t getporter/workshop:$PERMALINK build/images/workshop
+fi
