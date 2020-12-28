fe72ff735cac43ef1d95e74990ba7c7796b96a25
Carolyn Van Slyck <me@carolynvanslyck.com>
2020-12-28 13:00:30 -0600

    Remove output hack from test
    The test was using os.Stdout swapping to capture output, which isn't
    necessary anymore
    
    Signed-off-by: Carolyn Van Slyck <me@carolynvanslyck.com>

--- tests/suppress_output_test.go
+++ tests/suppress_output_test.go
@@ -3,9 +3,6 @@
 package tests
 
 import (
-	"bytes"
-	"io"
-	"os"
 	"path/filepath"
 	"testing"
 
@@ -19,18 +16,6 @@ func TestSuppressOutput(t *testing.T) {
 	p.SetupIntegrationTest()
 	defer p.CleanupIntegrationTest()
 	p.Debug = false
-
-	// Currently, the default docker driver prints directly to stdout instead of to the given writer
-	// Hence, the need to swap out stdout/stderr just for this test, to capture output under test
-	stdout := os.Stdout
-	stderr := os.Stderr
-	defer func() {
-		os.Stdout = stdout
-		os.Stderr = stderr
-	}()
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-	os.Stderr = w
 
 	p.TestConfig.TestContext.AddTestDirectory(filepath.Join(p.TestDir, "testdata/bundles/suppressed-output-example"), ".")
 
@@ -64,17 +49,7 @@ func TestSuppressOutput(t *testing.T) {
 	err = p.UninstallBundle(uninstallOpts)
 	require.NoError(t, err)
 
-	// Copy the output in a separate goroutine so printing can't block indefinitely
-	outC := make(chan string)
-	go func() {
-		var buf bytes.Buffer
-		io.Copy(&buf, r)
-		outC <- buf.String()
-	}()
-
-	// Read our faked cmd output
-	w.Close()
-	gotCmdOutput := <-outC
+	gotCmdOutput := p.TestConfig.TestContext.GetOutput()
 
 	require.NotContains(t, gotCmdOutput, "Hello World!", "expected command output to be suppressed from Install step")
 	require.NotContains(t, gotCmdOutput, "Error!", "expected command output to be suppressed from Invoke step")
