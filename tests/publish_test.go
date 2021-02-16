4359277d300174e4a3d7caa843334ba3c1941468
Vaughn Dice <vadice@microsoft.com>
2021-02-16 12:09:11 -0700

    fix(publish.go): get original image value after loading internal manifest (#1450)
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>

---
+++ tests/publish_test.go
@@ -0,0 +1,40 @@
+// +build integration
+
+package tests
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"get.porter.sh/porter/pkg/porter"
+)
+
+func TestPublish_BuildWithVersionOverride(t *testing.T) {
+	t.Parallel()
+
+	p := porter.NewTestPorter(t)
+	p.SetupIntegrationTest()
+	defer p.CleanupIntegrationTest()
+	p.Debug = false
+
+	// Create a bundle
+	err := p.Create()
+	require.NoError(t, err)
+
+	// Build with version override
+	buildOpts := porter.BuildOptions{}
+	buildOpts.Version = "0.0.0"
+	err = p.Build(buildOpts)
+	require.NoError(t, err)
+
+	publishOpts := porter.PublishOptions{}
+	publishOpts.Registry = "localhost:5000"
+	err = publishOpts.Validate(p.Context)
+	require.NoError(p.T(), err, "validation of publish opts for bundle failed")
+
+	// Confirm that publish picks up the version override
+	// (Otherwise, image tagging and publish will fail)
+	err = p.Publish(publishOpts)
+	require.NoError(p.T(), err, "publish of bundle failed")
+}
