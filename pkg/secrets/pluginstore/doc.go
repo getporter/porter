04c99258c160046872110c4928bdc8231f792ab1
div_bhasin <divbest99@gmail.com>
2021-05-23 15:23:14 -0400

    remove 2 additional comments
    Signed-off-by: div_bhasin <divbest99@gmail.com>

--- pkg/secrets/pluginstore/doc.go
+++ pkg/secrets/pluginstore/doc.go
@@ -1,3 +1,3 @@
 // Package pluginstore is an internal Porter package that implements the
 // secret.Store interface via plugins.
-package pluginstore // import "get.porter.sh/porter/pkg/secrets/pluginstore
+package pluginstore
