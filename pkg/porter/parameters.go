fea7bb99e6a1707cd83ef89ce28c7d5dd4262e20
Divyam Bhasin <divbest99@gmail.com>
2021-06-02 12:27:16 -0400

    Count only the parameters in the parameter set (#1608)
    * fixed to count only internal params
    
    Signed-off-by: div_bhasin <divbest99@gmail.com>
    
    * fix test
    
    Signed-off-by: div_bhasin <divbest99@gmail.com>
    
    * fix mistake
    
    Signed-off-by: div_bhasin <divbest99@gmail.com>

--- pkg/porter/parameters.go
+++ pkg/porter/parameters.go
@@ -129,7 +129,15 @@ func (p *Porter) GenerateParameters(opts ParameterOptions) error {
 		Bundle: bundle,
 	}
 	fmt.Fprintf(p.Out, "Generating new parameter set %s from bundle %s\n", genOpts.Name, bundle.Name)
-	fmt.Fprintf(p.Out, "==> %d parameters declared for bundle %s\n", len(bundle.Parameters), bundle.Name)
+	numExternalParams := 0
+
+	for name := range bundle.Parameters {
+		if !parameters.IsInternal(name, bundle) {
+			numExternalParams += 1
+		}
+	}
+
+	fmt.Fprintf(p.Out, "==> %d parameters declared for bundle %s\n", numExternalParams, bundle.Name)
 
 	pset, err := genOpts.GenerateParameters()
 	if err != nil {
