f24c6fb94236da4b9c62d6e5b9466a30893ba259
thisisommore <ommore501@gmail.com>
2021-08-02 09:15:34 +0000

    Don't use with
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/with#ambiguity_contra
    
    Signed-off-by: thisisommore <ommore501@gmail.com>

--- docs/themes/porter/static/js/custom/copy_code.js
+++ docs/themes/porter/static/js/custom/copy_code.js
@@ -2,26 +2,22 @@ document.querySelectorAll("pre").forEach(ele => {
     var codeElement = ele.querySelector("code")
 
     if (codeElement != null) {
-        with (document) {
-            var copyButtonContainer = createElement('div')
-            copyButtonContainer.classList.add("copy-button-container")
-            var copyButton = createElement('button');
-            copyButtonContainer.append(copyButton)
-        }
+        var copyButtonContainer = document.createElement('div')
+        copyButtonContainer.classList.add("copy-button-container")
+        var copyButton = document.createElement('button');
+        copyButtonContainer.append(copyButton)
 
-        with (copyButton) {
-            className = "copy-button"
-            innerText = 'copy';
+        copyButton.className = "copy-button"
+        copyButton.innerText = 'copy';
 
-            addEventListener('click', () => {                
-                navigator.clipboard.writeText(codeElement.innerText).then(e => {
-                    innerText = "copied"
-                }).catch(e => {
-                    innerText = "error"
-                })
-                setTimeout(() => innerText = "copy", 900)
-            });
-        }
+        copyButton.addEventListener('click', () => {
+            navigator.clipboard.writeText(codeElement.innerText).then(e => {
+                copyButton.innerText = "copied"
+            }).catch(e => {
+                copyButton.innerText = "error"
+            })
+            setTimeout(() => innerText = "copy", 900)
+        });
 
         ele.parentElement.insertBefore(copyButtonContainer, ele)
     }
