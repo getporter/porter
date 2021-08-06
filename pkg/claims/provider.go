fd8f59eb7db66842cc74f3479fe98724d85fffcc
Carolyn Van Slyck <me@carolynvanslyck.com>
2021-08-06 15:17:50 -0500

    Remove commented out code and document claimstore interface
    Signed-off-by: Carolyn Van Slyck <me@carolynvanslyck.com>

--- pkg/claims/provider.go
+++ pkg/claims/provider.go
@@ -2,56 +2,48 @@ package claims
 
 // Provider is an interface for interacting with Porter's claim data.
 type Provider interface {
+	// InsertInstallation saves a new Installation document.
 	InsertInstallation(installation Installation) error
+
+	// InsertRun saves a new Run document.
 	InsertRun(run Run) error
+
+	// InsertResult saves a new Result document.
 	InsertResult(result Result) error
+
+	// InsertOutput saves a new Output document.
 	InsertOutput(output Output) error
 
+	// UpdateInstallation saves changes to an existing Installation document.
 	UpdateInstallation(installation Installation) error
+
+	// UpsertRun saves changes a Run document, creating it if it doesn't already exist.
 	UpsertRun(run Run) error
+
+	// UpsertInstallation saves an Installation document, creating it if it doesn't already exist.
 	UpsertInstallation(installation Installation) error
 
+	// GetInstallation retrieves an Installation document by name.
 	GetInstallation(namespace string, name string) (Installation, error)
 
-	// ListInstallations returns Installations sorted in ascending order.
+	// ListInstallations returns Installation documents sorted in ascending order by name.
 	ListInstallations(namespace string) ([]Installation, error)
 
+	// ListRuns returns Run documents sorted in ascending order by ID.
 	ListRuns(namespace string, installation string) ([]Run, error)
+
+	// ListResults returns Result documents sorted in ascending order by ID.
 	ListResults(runID string) ([]Result, error)
+
+	// ListOutputs returns Output documents sorted in ascending order by name.
 	ListOutputs(resultID string) ([]Output, error)
 
+	// GetRun returns a Run document by ID.
 	GetRun(id string) (Run, error)
+
+	// GetResult returns a Result document by ID.
 	GetResult(id string) (Result, error)
 
-	/*
-		// ListClaims returns ToCNAB IDs associated with an Installation sorted in ascending order.
-		ListClaims(installation string) ([]string, error)
-
-		// ListResults returns Result IDs associated with a ToCNAB, sorted in ascending order.
-		ListResults(claimID string) ([]string, error)
-
-		// ListOutputs returns the names of outputs associated with a result that
-		// have been persisted. It is possible for results to have outputs that were
-		// generated but not persisted see
-		// https://github.com/cnabio/cnab-spec/blob/cnab-claim-1.0.0-DRAFT+b5ed2f3/400-claims.md#outputs
-		// for more information.
-		ListOutputs(resultID string) ([]string, error)
-
-		// ReadInstallation returns the specified Installation with all Claims and their Results loaded.
-		ReadInstallation(installation string) (Installation, error)
-
-		// ReadInstallationStatus returns the specified Installation with the last ToCNAB and its last Result loaded.
-		ReadInstallationStatus(installation string) (Installation, error)
-
-		// ReadAllInstallationStatus returns all Installations with the last ToCNAB and its last Result loaded.
-		ReadAllInstallationStatus() ([]Installation, error)
-
-		// ReadClaim returns the specified ToCNAB.
-		ReadClaim(claimID string) (ToCNAB, error)
-
-		// ReadAllClaims returns all claims associated with an Installation, sorted in ascending order.
-		ReadAllClaims(installation string) ([]ToCNAB, error)
-	*/
 	// GetLastRun returns the last run of an Installation.
 	GetLastRun(namespace string, installation string) (Run, error)
 
@@ -63,52 +55,12 @@ type Provider interface {
 	// associated with the installation.
 	GetLastOutputs(namespace string, installation string) (Outputs, error)
 
-	/*
-		// ReadResult returns the specified Result.
-		ReadResult(resultID string) (Result, error)
-
-		// ReadAllResult returns all results associated with a ToCNAB, sorted in ascending order.
-		ReadAllResults(claimID string) ([]Result, error)
-
-		// ReadLastResult returns the last result associated with a ToCNAB.
-		ReadLastResult(claimID string) (Result, error)
-
-		// ReadAllOutputs returns the most recent (last) value of each Output associated
-		// with the installation.
-		ReadLastOutputs(installation string) (Outputs, error)
-
-		// GetLastOutput returns the most recent value (last) of the specified Output associated
-		// with the installation.
-		GetLastOutput(installation string, name string) (Output, error)
-
-		// ReadOutput returns the contents of the named output associated with the specified Result.
-		ReadOutput(claim ToCNAB, result Result, outputName string) (Output, error)
-
-		// SaveClaim persists the specified claim.
-		// Associated results, ToCNAB.Results, must be persisted separately with SaveResult.
-		SaveClaim(claim ToCNAB) error
-
-		// SaveResult persists the specified result.
-		SaveResult(result Result) error
-
-		// SaveOutput persists the output, encrypting the value if defined as
-		// sensitive (write-only) in the bundle.
-		SaveOutput(output Output) error
-
-		// DeleteInstallation removes all data associated with the specified installation.
-		DeleteInstallation(installation string) error
-
-		// DeleteClaim removes all data associated with the specified claim.
-		DeleteClaim(claimID string) error
-
-		// DeleteResult removes all data associated with the specified result.
-		DeleteResult(resultID string) error
-
-		// DeleteOutput removes an output persisted with the specified result.
-		DeleteOutput(resultID string, outputName string) error
-	*/
-
+	// RemoveInstallation by its name.
 	RemoveInstallation(namespace string, name string) error
+
+	// GetLogs returns the logs from the specified Run.
 	GetLogs(runID string) (logs string, hasLogs bool, err error)
+
+	// GetLastLogs returns the logs from the last run of an Installation.
 	GetLastLogs(namespace string, installation string) (logs string, hasLogs bool, err error)
 }
