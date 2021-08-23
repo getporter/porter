5a1cf778d0828d8f018c16fafd7dec9216c4d6c2
Vaughn Dice <vadice@microsoft.com>
2021-08-23 12:27:45 -0600

    chore(examples/azure-terraform): updates per forthcoming 0.8.0 release
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>

--- examples/azure-terraform/terraform/main.tf
+++ examples/azure-terraform/terraform/main.tf
@@ -1,12 +1,18 @@
 provider "azurerm" {
-    version = "~>1.41.0"
-    subscription_id = var.subscription_id
-    client_id       = var.client_id
-    client_secret   = var.client_secret
-    tenant_id       = var.tenant_id
+  features {}
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 terraform {
-    required_version = "~>0.12.17"
-    backend "azurerm" {}
+  required_version = "1.0.4"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.72.0"
+    }
+  }
+  backend "azurerm" {}
 }
