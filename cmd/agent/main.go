04aad4500489db2c2f632d66a512a906ed07bf23
Carolyn Van Slyck <me@carolynvanslyck.com>
2021-11-22 09:04:33 -0600

    Move agent's run.sh into Go
    Since the nonroot distroless image doesn't have a shell, we can't use
    run.sh to copy the porter config files into PORTER_HOME at container
    start. I have implemented that in Go (sorry it's a lot vs what good ole
    cp did for us under the hood).
    
    One trick is that when /porter-config is mounted into the container by
    k8s, it uses symlinks like this:
    
    /porter-config
      ..data/porter.config
      porter.config -> ..data/porter.config
    
    So it's not a straightforward as you'd think at first glance.
    
    Signed-off-by: Carolyn Van Slyck <me@carolynvanslyck.com>

---
+++ cmd/agent/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"get.porter.sh/porter/pkg/agent"
+)
+
+// The porter agent wraps the porter cli,
+// handling coping config files from a mounted
+// volume into PORTER_HOME
+func main() {
+	porterHome := os.Getenv("PORTER_HOME")
+	if porterHome == "" {
+		porterHome = "/app/.porter"
+	}
+	porterConfig := os.Getenv("PORTER_CONFIG")
+	if porterConfig == "" {
+		porterConfig = "/porter-config"
+	}
+	err, run := agent.Execute(os.Args[1:], porterHome, porterConfig)
+	if err != nil {
+		if !run {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		os.Exit(1)
+	}
+}
