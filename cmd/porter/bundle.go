c1ea18b117c099474c2580043337f0e84dbf0d6b
Yingrong Zhao <yingrong.zhao@gmail.com>
2022-06-09 18:19:01 -0400

    clearer help text
    Signed-off-by: Yingrong Zhao <yingrong.zhao@gmail.com>

--- cmd/porter/bundle.go
+++ cmd/porter/bundle.go
@@ -80,9 +80,9 @@ The docker driver builds the bundle image using the local Docker host. To use a 
 	f.StringVar(&opts.Name, "name", "", "Override the bundle name")
 	f.StringVar(&opts.Version, "version", "", "Override the bundle version")
 	f.StringVarP(&opts.File, "file", "f", "",
-		"Path to the Porter manifest. When a relative path is provided, the path is relative to the build context directory. Defaults to `porter.yaml` in the current directory.")
+		"Path to the Porter manifest. The path is relative to the build context directory. Defaults to porter.yaml in the current directory.")
 	f.StringVarP(&opts.Dir, "dir", "d", "",
-		"Path to the build context directory where all bundle assets are located.")
+		"Path to the build context directory where all bundle assets are located. Defaults to the current directory.")
 	f.StringVar(&opts.Driver, "driver", porter.BuildDriverDefault,
 		fmt.Sprintf("Driver for building the invocation image. Allowed values are: %s", strings.Join(porter.BuildDriverAllowedValues, ", ")))
 	f.MarkHidden("driver") // Hide the driver flag since there aren't any choices to make right now
