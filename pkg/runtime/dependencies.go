ffab789da45c3510a195399046b1249e6ad177dc
epsxy <epsxy@users.noreply.github.com>
2022-06-21 00:04:56 +0200

    refactor: remove pkg/errors dependencies under `pkg/runtime`
    Signed-off-by: epsxy <epsxy@users.noreply.github.com>

--- pkg/runtime/dependencies.go
+++ pkg/runtime/dependencies.go
@@ -1,10 +1,10 @@
 package runtime
 
 import (
+	"fmt"
 	"path/filepath"
 
 	"get.porter.sh/porter/pkg/portercontext"
-	"github.com/pkg/errors"
 )
 
 const (
@@ -18,5 +18,5 @@ func GetDependencyDefinitionPath(alias string) string {
 func GetDependencyDefinition(c *portercontext.Context, alias string) ([]byte, error) {
 	f := GetDependencyDefinitionPath(alias)
 	data, err := c.FileSystem.ReadFile(f)
-	return data, errors.Wrapf(err, "error reading bundle definition for %s at %s", alias, f)
+	return data, fmt.Errorf("error reading bundle definition for %s at %s: %w", alias, f, err)
 }
