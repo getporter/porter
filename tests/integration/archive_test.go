f6ab3d1dc0653c288205201cf903e6f65f8c6a53
Yingrong Zhao <yingrong.zhao@gmail.com>
2022-06-20 11:54:34 -0400

    use relocation map for pulling images during archive (#2149)
    * use relocation map for pulling images during archive
    
    Signed-off-by: Yingrong Zhao <yingrong.zhao@gmail.com>
    
    * simplify code
    
    Signed-off-by: Yingrong Zhao <yingrong.zhao@gmail.com>
    
    * simplify code
    
    Signed-off-by: Yingrong Zhao <yingrong.zhao@gmail.com>
    
    * include relocation-mapping.json into archived bundle
    
    Signed-off-by: Yingrong Zhao <yingrong.zhao@gmail.com>
    
    * add tests
    
    Signed-off-by: Yingrong Zhao <yingrong.zhao@gmail.com>
    
    * add smoke test for archive
    
    Signed-off-by: Yingrong Zhao <yingrong.zhao@gmail.com>
    
    * improve tests
    
    Signed-off-by: Yingrong Zhao <yingrong.zhao@gmail.com>
    
    * improve tests
    
    Signed-off-by: Yingrong Zhao <yingrong.zhao@gmail.com>
    
    * make sure to use the right repository
    
    Signed-off-by: Yingrong Zhao <yingrong.zhao@gmail.com>

--- tests/integration/archive_test.go
+++ tests/integration/archive_test.go
@@ -5,13 +5,19 @@ package integration
 
 import (
 	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"io"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"get.porter.sh/porter/pkg"
 	"get.porter.sh/porter/pkg/porter"
 	"get.porter.sh/porter/tests"
+	"github.com/cnabio/cnab-go/bundle/loader"
+	"github.com/cnabio/cnab-go/packager"
+	"github.com/cnabio/cnab-to-oci/relocation"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -25,7 +31,7 @@ func TestArchive(t *testing.T) {
 	p.Debug = false
 
 	// Use a fixed bundle to work with so that we can rely on the registry and layer digests
-	const reference = "getporter/mysql:v0.1.4"
+	const reference = "ghcr.io/getporter/examples/whalegap:v0.2.0"
 
 	// Archive bundle
 	archive1Opts := porter.ArchiveOptions{}
@@ -40,6 +46,7 @@ func TestArchive(t *testing.T) {
 	info, err := p.FileSystem.Stat(archiveFile1)
 	require.NoError(p.T(), err)
 	tests.AssertFilePermissionsEqual(t, archiveFile1, pkg.FileModeWritable, info.Mode())
+	containsRequiredMetadata(p, archiveFile1)
 
 	hash1 := getHash(p, archiveFile1)
 
@@ -55,14 +62,14 @@ func TestArchive(t *testing.T) {
 
 	err = p.Archive(ctx, archive2Opts)
 	require.NoError(t, err, "Second archive failed")
-
 	assert.Equal(p.T(), hash1, getHash(p, archiveFile2), "shasum of archive did not stay the same on the second call to archive")
+	containsRequiredMetadata(p, archiveFile2)
 
 	// Publish bundle from archive, with new reference
 	publishFromArchiveOpts := porter.PublishOptions{
 		ArchiveFile: archiveFile1,
 		BundlePullOptions: porter.BundlePullOptions{
-			Reference: fmt.Sprintf("localhost:5000/archived-mysql:v0.1.4"),
+			Reference: fmt.Sprintf("localhost:5000/archived-whalegap:v0.2.0"),
 		},
 	}
 	err = publishFromArchiveOpts.Validate(p.Context)
@@ -82,4 +89,28 @@ func getHash(p *porter.TestPorter, path string) string {
 	require.NoError(p.T(), err, "hashing of archive failed")
 
 	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func containsRequiredMetadata(p *porter.TestPorter, path string) {
+	tmpDir, err := p.FileSystem.TempDir("", "porter-integration-tests")
+	require.NoError(p.T(), err)
+	defer p.FileSystem.RemoveAll(tmpDir)
+
+	source := p.FileSystem.Abs(path)
+	l := loader.NewLoader()
+	imp := packager.NewImporter(source, tmpDir, l)
+	err = imp.Import()
+	require.NoError(p.T(), err, "opening archive failed")
+
+	relocMapBytes, err := p.FileSystem.ReadFile(filepath.Join(tmpDir, strings.TrimSuffix(filepath.Base(source), ".tgz"), "relocation-mapping.json"))
+	require.NoError(p.T(), err)
+
+	// make sure the relocation map contains the expected image
+	relocMap := relocation.ImageRelocationMap{}
+	require.NoError(p.T(), json.Unmarshal(relocMapBytes, &relocMap))
+	require.Equal(p.T(), relocMap["carolynvs/whalesayd@sha256:8b92b7269f59e3ed824e811a1ff1ee64f0d44c0218efefada57a4bebc2d7ef6f"], "ghcr.io/getporter/examples/whalegap@sha256:8b92b7269f59e3ed824e811a1ff1ee64f0d44c0218efefada57a4bebc2d7ef6f", relocMap)
+	require.Equal(p.T(), relocMap["ghcr.io/getporter/examples/whalegap:2bed6d7bf087c159835ddfac5838fd34@sha256:5ada057d9b24c443d9fb0240b49c13a5a36a11d5f4dda8adaaa2ec74e39c0d24"], "ghcr.io/getporter/examples/whalegap@sha256:5ada057d9b24c443d9fb0240b49c13a5a36a11d5f4dda8adaaa2ec74e39c0d24", relocMap)
+
+	_, err = p.FileSystem.Stat(filepath.Join(tmpDir, strings.TrimSuffix(filepath.Base(source), ".tgz"), "bundle.json"))
+	require.NoError(p.T(), err)
 }
