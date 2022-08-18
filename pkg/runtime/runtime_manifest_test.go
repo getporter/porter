54785bac6d1a46ba51d8665fa5f6b143702c2813
Steven Gettys <s.gettys@f5.com>
2022-08-18 16:09:56 -0700

    Removed unnecessary test utility
    Signed-off-by: Steven Gettys <s.gettys@f5.com>

--- pkg/runtime/runtime_manifest_test.go
+++ pkg/runtime/runtime_manifest_test.go
@@ -1,8 +1,6 @@
 package runtime
 
 import (
-	"archive/tar"
-	"compress/gzip"
 	"context"
 	"fmt"
 	"io"
@@ -15,7 +13,6 @@ import (
 	"get.porter.sh/porter/pkg/manifest"
 	"get.porter.sh/porter/pkg/portercontext"
 	"get.porter.sh/porter/tests"
-	"github.com/carolynvs/aferox"
 	"github.com/cnabio/cnab-go/bundle"
 	"github.com/cnabio/cnab-go/bundle/definition"
 	"github.com/cnabio/cnab-to-oci/relocation"
@@ -30,68 +27,6 @@ func runtimeManifestFromStepYaml(t *testing.T, pCtx *portercontext.TestContext, 
 	require.NoError(t, err, "ReadManifest failed")
 	cfg := NewConfigFor(pCtx.Context)
 	return NewRuntimeManifest(cfg, cnab.ActionInstall, m)
-}
-
-func createArchive(fs aferox.Aferox, files []string, buf io.Writer) error {
-	// Create new Writers for gzip and tar
-	// These writers are chained. Writing to the tar writer will
-	// write to the gzip writer which in turn will write to
-	// the "buf" writer
-	gw := gzip.NewWriter(buf)
-	defer gw.Close()
-	tw := tar.NewWriter(gw)
-	defer tw.Close()
-
-	// Iterate over files and add them to the tar archive
-	for _, file := range files {
-		err := addToArchive(fs, tw, file)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-func addToArchive(fs aferox.Aferox, tw *tar.Writer, filename string) error {
-	// Open the file which will be written into the archive
-	file, err := fs.Open(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	// Get FileInfo about our file providing file size, mode, etc.
-	info, err := file.Stat()
-	if err != nil {
-		return err
-	}
-
-	// Create a tar Header from the FileInfo data
-	header, err := tar.FileInfoHeader(info, info.Name())
-	if err != nil {
-		return err
-	}
-
-	// Use full path as name (FileInfoHeader only takes the basename)
-	// If we don't do this the directory strucuture would
-	// not be preserved
-	// https://golang.org/src/archive/tar/common.go?#L626
-	header.Name = filename
-
-	// Write file header to the tar archive
-	err = tw.WriteHeader(header)
-	if err != nil {
-		return err
-	}
-
-	// Copy file content to tar archive
-	_, err = io.Copy(tw, file)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func TestResolveMapParam(t *testing.T) {
