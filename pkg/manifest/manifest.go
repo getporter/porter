d864737236132667fe1bba14888afb37c9c6d431
Carolyn Van Slyck <me@carolynvanslyck.com>
2022-09-02 14:45:15 -0500

    Make identifiable temporary invocation image tags
    When we publish a bundle, we have to push the invocation image temporarily to a tag, so that we can then retrieve its repository digest. Right now the tag is the hash of the bundle reference, which is impossible for someone maintaining a registry to identify and clean up.
    
    This changes the format of the temporary invocation image tag to:
    
    porter-HASH
    
    For example:
    
    The input text for the invocation image hash is the location of the bundle:
    
    ghcr.io/getporter/examples/porter-hello:v0.2.0
    
    which results in a md5 hash of 3cb284ae76addb8d56b52bb7d6838351, so the resulting temporary location where we push the invocation image is:
    
    ghcr.io/getporter/examples/porter-hello:porter-3cb284ae76addb8d56b52bb7d6838351
    
    I have updated all our code to use the same function to generate the temporary tag, even for referenced images which previously used a different method of computing the hash. Referenced images now compute the hash based on the original location of the image.
    
    So if a bundle referenced nginx:v1.0.0 and is used in a bundle located at example.com/mybuns, the temporary location for nginx would be:
    
    examlpe.com/mybuns:porter-55135915b5d3b5cf91b7305ec6a76d6e
    
    where the hash uses the location of the original image (nginx:v1.0.0)
    
    Signed-off-by: Carolyn Van Slyck <me@carolynvanslyck.com>

--- pkg/manifest/manifest.go
+++ pkg/manifest/manifest.go
@@ -2,8 +2,6 @@ package manifest
 
 import (
 	"context"
-	"crypto/md5"
-	"encoding/hex"
 	"errors"
 	"fmt"
 	"io/ioutil"
@@ -947,18 +945,12 @@ func (m *Manifest) SetInvocationImageAndReference(ref string) error {
 		m.Reference = bundleRef.String()
 	}
 
-	imageName, err := cnab.ParseOCIReference(bundleRef.Repository())
+	installerImage, err := cnab.CalculateTemporaryImageTag(bundleRef)
 	if err != nil {
-		return fmt.Errorf("could not set invocation image to %q: %w", bundleRef.Repository(), err)
+		return err
 	}
-	referenceHash := md5.Sum([]byte(bundleRef.String()))
-	imgTag := hex.EncodeToString(referenceHash[:])
-	imageRef, err := imageName.WithTag(imgTag)
-	if err != nil {
-		return fmt.Errorf("could not set invocation image tag to %q: %w", dockerTag, err)
-	}
-	m.Image = imageRef.String()
 
+	m.Image = installerImage.String()
 	return nil
 }
 
