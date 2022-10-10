0d1a10a130528caf4b1a00538a09520acd24c9dc
Carolyn Van Slyck <me@carolynvanslyck.com>
2022-10-10 15:46:23 -0500

    Fix macos install check to not use docker
    The macos install script check is failing because it calls porter list, which requires docker to be installed on the CI agent. If we had a plugin that didn't require docker configured instead, we could revert this and use that instead. But for now I'm only calling porter version and skipping calling porter list in the macos install check script.
    
    The linux and windows checks will keep running porter list, which vets that our default config works.
    
    Signed-off-by: Carolyn Van Slyck <me@carolynvanslyck.com>

--- scripts/test/test-linux-install.sh
+++ scripts/test/test-linux-install.sh
@@ -5,6 +5,7 @@ set -xeuo pipefail
 export PATH=$PATH:~/.porter
 
 PORTER_VERSION=canary ./scripts/install/install-linux.sh
+porter version
 porter list
 
 PORTER_VERSION=v0.23.0-beta.1 ./scripts/install/install-linux.sh
