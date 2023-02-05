24b2a5a3205a3229932c6e6b75671b50646fbaad
Chioma Onyekpere <chioma.onyekpere@gmail.com>
2023-02-05 19:33:54 +0300

    Validate reserved names (#2434)
    * check reserved parameter
    
    * create the isIn func
    
    * add a linter check
    
    * param check to function
    
    * param check to function
    
    * check parameter name does not have a reserved prefix
    
    * remove redundant variable
    
    * add test cases
    
    * update level error
    
    * update test level error
    
    * update test level error
    
    * change all porter prefix warning to error use cases
    
    ---------
    
    Signed-off-by: Simpcyclassy <chioma.onyekpere@gmail.com>
    Signed-off-by: Chioma Onyekpere <chioma.onyekpere@gmail.com>

--- pkg/linter/linter_test.go
+++ pkg/linter/linter_test.go
@@ -72,4 +72,85 @@ func TestLinter_Lint(t *testing.T) {
 		require.Len(t, results, 0, "linter should ignore mixins that doesn't support the lint command")
 	})
 
+	testcases := []struct {
+		Name			string
+		ParameterName	string
+	}{
+		{
+			Name: "does not use a reserved prefix",
+			ParameterName: "porter-debug",
+		},
+		{
+			Name: "is case insensitive and does not use reserved prefix even if mixed case ",
+			ParameterName: "poRteR_lint",
+		},
+		{
+			Name: "is case insensitive and does not use reserved prefix even if upper case ",
+			ParameterName: "PORTER_DEBUG",
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.Name, func(t *testing.T) {
+			cxt := portercontext.NewTestContext(t)
+			mixins := mixin.NewTestMixinProvider()
+			l := New(cxt.Context, mixins)
+			param := map[string]manifest.ParameterDefinition {
+				"A": {
+					Name: tc.ParameterName,
+				},
+			}
+	
+			m := &manifest.Manifest{
+				Parameters: param,
+			}
+			mixins.LintResults = Results{
+				{
+					Level: LevelError,
+					Location: Location {
+						Action: "",
+						Mixin: "",
+						StepNumber: 0,
+						StepDescription: "",
+				
+					},
+					Code: "porter-100",
+					Title: "Reserved name error",
+					Message: tc.ParameterName + " has a reserved prefix. Parameters cannot start with porter- or porter_",
+					URL: "https://getporter.org/reference/linter/#porter-100",
+				},
+			}
+	
+			results, err := l.Lint(ctx, m)
+			require.NoError(t, err, "Lint failed")
+			require.Len(t, results, 1, "linter should have returned 1 result")
+			require.Equal(t, mixins.LintResults, results, "unexpected lint results")
+		})
+	}
+
+	t.Run("linter runs successfully if parameter does not use a reserved prefix", func(t *testing.T) {
+		cxt := portercontext.NewTestContext(t)
+		mixins := mixin.NewTestMixinProvider()
+		l := New(cxt.Context, mixins)
+		param := map[string]manifest.ParameterDefinition {
+			"A": {
+				Name: "successful",
+			},
+		}
+
+		m := &manifest.Manifest{
+			Parameters: param,
+		}
+		mixins.LintResults = Results{
+			{
+				Level: LevelError,
+				Code:  "exec-101",
+				Title: "warning stuff isn't working",
+			},
+		}
+
+		results, err := l.Lint(ctx, m)
+		require.NoError(t, err, "Lint failed")
+		require.Len(t, results, 0, "linter should have returned 1 result")
+	})
 }
