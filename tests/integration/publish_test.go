8c1f0653c0f1a17afae9a0831ad883506d1f3e89
Carolyn Van Slyck <me@carolynvanslyck.com>
2023-04-24 14:12:31 -0500

    Add regression test for autobuild-disabled via config
    This adds a regression test for setting autobuild-disabled to true in a config file and having the CLI correctly pick up the setting.
    
    Signed-off-by: Carolyn Van Slyck <me@carolynvanslyck.com>

--- tests/integration/publish_test.go
+++ tests/integration/publish_test.go
@@ -4,8 +4,10 @@ package integration
 
 import (
 	"fmt"
+	"path"
 	"testing"
 
+	"get.porter.sh/porter/pkg/yaml"
 	"get.porter.sh/porter/tests"
 	"get.porter.sh/porter/tests/tester"
 	"github.com/stretchr/testify/require"
@@ -24,6 +26,15 @@ func TestPublish(t *testing.T) {
 	_, _, err = test.RunPorter("publish", "--autobuild-disabled")
 	require.ErrorContains(t, err, "Skipping autobuild because --autobuild-disabled was specified")
 
+	// Try again with autobuild disabled via a config setting instead of a flag
+	// This is a regression test for https://github.com/getporter/porter/issues/2735
+	test.EditYaml(path.Join(test.PorterHomeDir, "config.yaml"), func(yq *yaml.Editor) error {
+		return yq.SetValue("autobuild-disabled", "true")
+	})
+	_, output, err := test.RunPorter("publish")
+	fmt.Println(output)
+	require.ErrorContains(t, err, "Skipping autobuild because --autobuild-disabled was specified")
+
 	// Build with version override
 	test.RequirePorter("build", "--version=0.0.0")
 
@@ -33,6 +44,6 @@ func TestPublish(t *testing.T) {
 
 	// Confirm that publish picks up the version override
 	// Use an insecure registry to validate that we can publish to one
-	_, output := test.RequirePorter("publish", "--registry", reg.String(), "--insecure-registry")
+	_, output = test.RequirePorter("publish", "--registry", reg.String(), "--insecure-registry")
 	tests.RequireOutputContains(t, output, fmt.Sprintf("Bundle %s/porter-hello:v0.0.0 pushed successfully", reg))
 }
