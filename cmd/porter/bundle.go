13f7c574ab312ffe47cb9541bcca53d45c8c4ace
Aleksey Barabanov <alekseybb@gmail.com>
2023-06-21 16:52:59 +0300

    fix merge conflict bundle.go
    Signed-off-by: Aleksey Barabanov <alekseybb@gmail.com>

--- cmd/porter/bundle.go
+++ cmd/porter/bundle.go
@@ -171,10 +171,6 @@ Note: if overrides for registry/tag/reference are provided, this command only re
 		"viper-key": {"force-overwrite"},
 	}
 	f.BoolVar(&opts.AutoBuildDisabled, "autobuild-disabled", false, "Do not automatically build the bundle from source when the last build is out-of-date.")
-	// Allow configuring the --autobuild-disabled flag with "autobuild-disabled" in the configuration file
-	cmd.Flag("autobuild-disabled").Annotations = map[string][]string{
-		"viper-key": {"autobuild-disabled"},
-	}
 
 	return &cmd
 }
