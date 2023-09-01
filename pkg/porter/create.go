851b9234dfa4c03594d609b410d8832e62808d1b
Ludvig Liljenberg <lliljenberg@microsoft.com>
2023-09-01 15:44:07 -0700

    fixes
    Signed-off-by: Ludvig Liljenberg <lliljenberg@microsoft.com>

--- pkg/porter/create.go
+++ pkg/porter/create.go
@@ -12,26 +12,34 @@ import (
 )
 
 // Create function creates a porter configuration in the specified directory or in the current directory if no directory is specified.
+// The bundle's name will be `bundleName`, or "porter_hello" if bundleName is the empty string
 func (p *Porter) Create(bundleName string) error {
 	// Normalize the bundleName by removing trailing slashes
 	bundleName = strings.TrimSuffix(bundleName, "/")
 
-	// Use the current directory if no directory is passed
-	if bundleName == "" {
-		bundleName = p.FileSystem.Getwd()
-	}
-
-	// Check if the directory in which bundle needs to be created already exists.
-	// If not, create the directory.
+	// Create given directory if it doesn't exist
 	_, err := os.Stat(bundleName)
 	if err != nil && errors.Is(err, os.ErrNotExist) {
-		err = os.Mkdir(bundleName, os.ModePerm)
+		err = os.MkdirAll(bundleName, os.ModePerm) // if given bundleName is foo/bar, will create both foo and bar directories
 		if err != nil {
 			return fmt.Errorf("failed to create directory for bundle: %w", err)
 		}
 	}
 
-	err = p.CopyTemplate(p.Templates.GetManifest, filepath.Join(bundleName, config.Name))
+	if bundleName == "" {
+		// create bundle with default name "porter_hello"
+		err = p.CopyTemplate(p.Templates.GetManifest, filepath.Join(bundleName, config.Name))
+	} else {
+		// create bundle with given name
+		err = p.CopyTemplate(func() ([]byte, error) {
+			content, err := p.Templates.GetManifest()
+			if err != nil {
+				return nil, err
+			}
+			strings.ReplaceAll(string(content), "porter_hello", bundleName)
+			return content, nil
+		}, filepath.Join(bundleName, config.Name))
+	}
 	if err != nil {
 		return err
 	}
