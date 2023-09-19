a498eb3ac60bce43edd22d0282c6df417ac383f8
schristoff <28318173+schristoff@users.noreply.github.com>
2023-09-19 14:56:12 -0600

    increase timeout?
    Signed-off-by: schristoff <28318173+schristoff@users.noreply.github.com>

--- pkg/config/logs.go
+++ pkg/config/logs.go
@@ -48,7 +48,7 @@ func (c TelemetryConfig) GetStartTimeout() time.Duration {
 	if timeout, err := time.ParseDuration(c.StartTimeout); err == nil {
 		return timeout
 	}
-	return 100 * time.Millisecond
+	return 300 * time.Millisecond
 }
 
 type LogLevel string
