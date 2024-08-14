e0a1b167efda9cb7b7b72ac732ad83158c629e15
John Cudd <jmcudd@gmail.com>
2024-08-14 10:59:55 -0400

    Added bundle script
    Signed-off-by: John Cudd <jmcudd@gmail.com>

---
+++ scripts/bundle/bundle-linux.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# Installs the Porter CLI for a single user.
+# Environment Variables:
+# PORTER_HOME:      Location where Porter is installed (defaults to ~/.porter).
+# PORTER_MIRROR:    Base URL for downloading Porter assets (defaults to https://cdn.porter.sh).
+# PORTER_VERSION:   The version of Porter assets to download (defaults to latest).
+export PORTER_HOME=${PORTER_HOME:-~/.porter}
+export PORTER_MIRROR=${PORTER_MIRROR:-https://cdn.porter.sh}
+PORTER_VERSION=${PORTER_VERSION:-latest}
+MONGO_IMAGE_FILE="mongo_image.tar"
+MONGO_IMAGE_URL="docker.io/library/mongo:4.0-xenial"
+TS=$(date +%Y%m%d_%H%M%S)
+
+BUNDLE_NAME=porter-download-$TS
+DOWNLOAD_DIR="/tmp/$BUNDLE_NAME"
+
+mkdir -p $DOWNLOAD_DIR
+
+echo "Pulling MongoDB image..."
+docker pull "$MONGO_IMAGE_URL"
+echo "Saving MongoDB image to $MONGO_IMAGE_FILE..."
+docker save -o "$DOWNLOAD_DIR/$MONGO_IMAGE_FILE" "$MONGO_IMAGE_URL"
+echo "Downloading Porter from $PORTER_MIRROR/$PORTER_VERSION/install-linux.sh"
+curl -fsSLo "$DOWNLOAD_DIR/install-linux.sh" "$PORTER_MIRROR/$PORTER_VERSION/install-linux.sh"
+echo "Downloading Porter from $PORTER_MIRROR/$PORTER_VERSION/porter-linux-amd64"
+curl -fsSLo "$DOWNLOAD_DIR/porter-linux-amd64" "$PORTER_MIRROR/$PORTER_VERSION/porter-linux-amd64"
+
+TAR_FILE="porter_bundle_$TS.tar.gz"
+echo "Creating tarball: $TAR_FILE..."
+tar -czvf "$TAR_FILE" -C /tmp "$(basename "$DOWNLOAD_DIR")" 
+
+echo "Bundle created: $TAR_FILE"
+echo "Usage Instructions:"
+echo "tar -xzf $TAR_FILE -C ."
+echo "cd $BUNDLE_NAME"
+echo "bash install-linux.sh"
+exit 0
--- scripts/install/install-linux.sh
+++ scripts/install/install-linux.sh
@@ -1,27 +1,36 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
-# Installs the porter CLI for a single user.
+# Installs the Porter CLI for a single user.
+# Environment Variables:
 # PORTER_HOME:      Location where Porter is installed (defaults to ~/.porter).
-# PORTER_MIRROR:    Base URL where Porter assets, such as binaries and atom feeds, are downloaded.
-#                   This lets you setup an internal mirror.
-# PORTER_VERSION:   The version of Porter assets to download.
-
+# PORTER_MIRROR:    Base URL for downloading Porter assets (defaults to https://cdn.porter.sh).
+# PORTER_VERSION:   The version of Porter assets to download (defaults to latest).
 export PORTER_HOME=${PORTER_HOME:-~/.porter}
 export PORTER_MIRROR=${PORTER_MIRROR:-https://cdn.porter.sh}
 PORTER_VERSION=${PORTER_VERSION:-latest}
-
+MONGO_IMAGE_FILE="mongo_image.tar"
 echo "Installing porter@$PORTER_VERSION to $PORTER_HOME from $PORTER_MIRROR"
-
 mkdir -p "$PORTER_HOME/runtimes"
-
-curl -fsSLo "$PORTER_HOME/porter" "$PORTER_MIRROR/$PORTER_VERSION/porter-linux-amd64"
+if [[ -f "./porter-linux-amd64" ]]; then
+    echo "Using existing porter-linux-amd64 binary from the current directory."
+    cp "./porter-linux-amd64" "$PORTER_HOME/porter"
+else
+    echo "Downloading Porter from $PORTER_MIRROR/$PORTER_VERSION/porter-linux-amd64"
+    curl -fsSLo "$PORTER_HOME/porter" "$PORTER_MIRROR/$PORTER_VERSION/porter-linux-amd64"
+fi
 chmod +x "$PORTER_HOME/porter"
 cp "$PORTER_HOME/porter" "$PORTER_HOME/runtimes/porter-runtime"
-echo Installed "$("$PORTER_HOME"/porter version)"
-
-"$PORTER_HOME/porter" mixin install exec --version "$PORTER_VERSION"
-
+echo "Installed Porter version: $("$PORTER_HOME/porter" version)"
+if [[ -f "$MONGO_IMAGE_FILE" ]]; then
+    echo "Loading MongoDB image from $MONGO_IMAGE_FILE..."
+    docker load -i "$MONGO_IMAGE_FILE"
+    if ! docker ps -q -f name=porter-mongodb-docker-plugin; then
+        echo "Running MongoDB container..."
+        docker run --name porter-mongodb-docker-plugin -d -p 27018:27017 -v mongodb_data:/data/db --restart always mongo:4.0-xenial
+    else
+        echo "MongoDB container is already running."
+    fi
+fi
 echo "Installation complete."
-echo "Add porter to your path by adding the following line to your ~/.profile and open a new terminal:"
-echo "export PATH=\$PATH:~/.porter"
+echo "Add Porter to your PATH by adding the following line to your ~/.bashrc or ~/.bash_profile and open a new terminal:"
+echo "export PATH=\$PATH:$PORTER_HOME"
